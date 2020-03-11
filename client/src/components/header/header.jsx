2cfb55390870a3f20afecc3a3416d7c0bce32c78
David Kim <hyomithetech@gmail.com>
2020-03-10 20:20:11 -0700

    Implement basic header and background

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import "./header.css";
 
 const Header = () => {
-  return <header class="header"></header>;
+  return (
+    <div>
+      <div className="logoContainer">
+        <div className="logoTop"></div>
+        <div className="logoCenter"></div>
+        <div className="logoBottom"></div>
+      </div>
+      <div className="header"></div>
+    </div>
+  );
 };
 
 export default Header;
