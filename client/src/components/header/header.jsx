fa93c98a0f880354d18c16c8184614b04e6ae825
David Kim <hyomithetech@gmail.com>
2020-03-11 16:54:24 -0700

    Delete loading logo

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -3,18 +3,11 @@ import "./header.css";
 
 const Header = () => {
   return (
-    <div>
-      <div className="logoContainer">
-        <div className="logoTop"></div>
-        <div className="logoCenter"></div>
-        <div className="logoBottom"></div>
-      </div>
-      <div className="header">
-        <h1 class="heading-primary">
-          <span class="heading-primary-main">Lucky Parking</span>
-          <span class="heading-primary-sub">Free parking around you</span>
-        </h1>
-      </div>
+    <div className="header">
+      <h1 className="heading-primary">
+        <span className="heading-primary-main">Lucky Parking</span>
+        <span className="heading-primary-sub">Free parking around you</span>
+      </h1>
     </div>
   );
 };
