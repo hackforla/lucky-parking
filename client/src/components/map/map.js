e653674d0fdc813b9648f1a0eabd1b79e49fc4ea
David Kim <hyomithetech@gmail.com>
2020-03-22 00:09:06 -0700

    Change configuration from Webpack to Create-react-app

---
+++ client/src/components/map/map.js
@@ -0,0 +1,77 @@
+import "mapbox-gl/dist/mapbox-gl.css";
+import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
+import React, { Component } from "react";
+import MapGL from "react-map-gl";
+import DeckGL, { GeoJsonLayer } from "deck.gl";
+import Geocoder from "react-map-gl-geocoder";
+
+const token = process.env.REACT_APP_MAP_BOX_TOKEN;
+
+class SearchableMap extends Component {
+  state = {
+    viewport: {
+      latitude: 0,
+      longitude: 0,
+      zoom: 1
+    },
+    searchResultLayer: null
+  };
+
+  mapRef = React.createRef();
+
+  handleViewportChange = viewport => {
+    this.setState({
+      viewport: { ...this.state.viewport, ...viewport }
+    });
+  };
+  // if you are happy with Geocoder default settings, you can just use handleViewportChange directly
+  handleGeocoderViewportChange = viewport => {
+    const geocoderDefaultOverrides = { transitionDuration: 1000 };
+
+    return this.handleViewportChange({
+      ...viewport,
+      ...geocoderDefaultOverrides
+    });
+  };
+
+  handleOnResult = event => {
+    this.setState({
+      searchResultLayer: new GeoJsonLayer({
+        id: "search-result",
+        data: event.result.geometry,
+        getFillColor: [255, 0, 0, 128],
+        getRadius: 1000,
+        pointRadiusMinPixels: 10,
+        pointRadiusMaxPixels: 10
+      })
+    });
+  };
+
+  render() {
+    const { viewport, searchResultLayer } = this.state;
+    return (
+      <div style={{ height: "100vh" }}>
+        <MapGL
+          ref={this.mapRef}
+          {...viewport}
+          mapStyle="mapbox://styles/mapbox/streets-v9"
+          width="100%"
+          height="90%"
+          onViewportChange={this.handleViewportChange}
+          mapboxApiAccessToken={token}
+        >
+          <Geocoder
+            mapRef={this.mapRef}
+            onResult={this.handleOnResult}
+            onViewportChange={this.handleGeocoderViewportChange}
+            mapboxApiAccessToken={token}
+            position="top-left"
+          />
+        </MapGL>
+        <DeckGL {...viewport} layers={[searchResultLayer]} />
+      </div>
+    );
+  }
+}
+
+export default SearchableMap;
