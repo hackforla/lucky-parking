88eff254175c801bdbcb146b883524f1acf864bb
David Kim <hyomithetech@gmail.com>
2020-03-26 17:36:44 -0700

    Create backend

---
+++ Lucky Parking Web App/client/src/components/map/mainMap.js
@@ -0,0 +1,55 @@
+import React from "react";
+import mapboxgl from "mapbox-gl";
+import "./mainMap.css";
+
+mapboxgl.accessToken = process.env.REACT_APP_MAP_BOX_TOKEN;
+
+class MainMap extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lng: 5,
+      lat: 34,
+      zoom: 2
+    };
+  }
+
+  componentDidMount() {
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [this.state.lng, this.state.lat],
+      zoom: this.state.zoom
+    });
+
+    map.on("move", () => {
+      this.setState({
+        lng: map.getCenter().lng.toFixed(4),
+        lat: map.getCenter().lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2)
+      });
+    });
+    map.addControl(
+      new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+      })
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="sidebarStyle">
+          <div>
+            Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom:{" "}
+            {this.state.zoom}
+          </div>
+        </div>
+        <div ref={el => (this.mapContainer = el)} className="mapContainer" />
+      </div>
+    );
+  }
+}
+
+export default MainMap;
