6a38adeb18f93805b14422e9af8d73661b3db46a
David Kim <hyomithetech@gmail.com>
2020-04-03 15:57:52 -0700

    Store confidentials in .env file

--- Lucky Parking Web App/database/index.js
+++ Lucky Parking Web App/database/index.js
@@ -1,10 +1,11 @@
 const { Client } = require("pg");
+require("dotenv").config();
 
 module.exports = pgClient = new Client({
-  user: "luckyparking",
-  password: "password",
-  database: "luckyParking",
-  port: 5432
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
 });
 
 pgClient
@@ -16,6 +17,6 @@ pgClient
     latitude float
   )`);
   })
-  .catch(err => console.error(err.stack));
+  .catch((err) => console.error(err.stack));
 
 // COPY location FROM '/Users/EuiHyo_Mi/Desktop/lucky-parking/data/test.csv' DELIMITER ',' CSV HEADER;
