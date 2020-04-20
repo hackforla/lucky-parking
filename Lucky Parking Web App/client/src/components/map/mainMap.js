31782eb52bc379ec25622e785567d2784c7c08bb
David Kim <hyomithetech@gmail.com>
2020-04-19 22:24:11 -0700

    Fix rendering issue

--- Lucky Parking Web App/client/src/components/map/mainMap.js
+++ Lucky Parking Web App/client/src/components/map/mainMap.js
@@ -9,8 +9,8 @@ const axios = require("axios");
 mapboxgl.accessToken = process.env.REACT_APP_MAP_BOX_TOKEN;
 
 class MainMap extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
       lng: -118.2,
       lat: 34.05,
@@ -21,8 +21,8 @@ class MainMap extends React.Component {
   }
 
   async componentDidMount() {
-    this.setState({
-      map: await new mapboxgl.Map({
+    await this.setState({
+      map: new mapboxgl.Map({
         container: this.mapContainer,
         style: "mapbox://styles/mapbox/streets-v11",
         center: [this.state.lng, this.state.lat],
@@ -47,116 +47,117 @@ class MainMap extends React.Component {
     );
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    // let points = document.getElementsByClassName("marker").length;
-
+  async componentDidUpdate(prevProps, prevState) {
     if (this.state.zoom >= 16) {
       if (
-        this.state.lat !== prevState.lat ||
-        this.state.lng !== prevState.lng
+        prevState.lng !== this.state.lng ||
+        prevState.lat !== this.state.lat ||
+        prevState.zoom !== this.state.zoom
       ) {
-        axios
+        await axios
           .get("/api/citation", {
             params: {
               longitude: this.state.lng,
               latitude: this.state.lat,
             },
           })
-          .then((data) => {
-            this.setState({
+          .then(async (data) => {
+            await this.setState({
               data: data.data,
             });
           })
           .catch((error) => {
             console.log(error);
           });
-
-        prevState.map.on("render", function () {
-          if (!prevState.map.getSource("places")) {
-            prevState.map.addSource("places", {
-              type: "geojson",
-              data: {
-                type: "FeatureCollection",
-                features: [
-                  {
-                    type: "Feature",
-                    properties: {
-                      description:
-                        "<strong>Make it Mount Pleasant</strong><p>Make it Mount Pleasant is a handmade and vintage market and afternoon of live entertainment and kids activities. 12:00-6:00 p.m.</p>",
-                      icon: "theatre",
-                    },
-                    geometry: {
-                      type: "Point",
-                      coordinates: [-118.2, 34.05],
-                    },
-                  },
-                ],
-              },
-            });
-
-            // Add a layer showing the places.
-            prevState.map.addLayer({
-              id: "places",
-              type: "symbol",
-              source: "places",
-              layout: {
-                "icon-image": "{icon}-15",
-                "icon-allow-overlap": true,
-              },
-            });
-
-            // Create a popup, but don't add it to the map yet.
-            var popup = new mapboxgl.Popup({
-              closeButton: false,
-              closeOnClick: false,
-            });
-
-            prevState.map.on("mouseenter", "places", function (e) {
-              // Change the cursor style as a UI indicator.
-              prevState.map.getCanvas().style.cursor = "pointer";
-
-              var coordinates = e.features[0].geometry.coordinates.slice();
-              var description = e.features[0].properties.description;
-
-              // Ensure that if the map is zoomed out such that multiple
-              // copies of the feature are visible, the popup appears
-              // over the copy being pointed to.
-              while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-                coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-              }
-
-              // Populate the popup and set its coordinates
-              // based on the feature found.
-              popup
-                .setLngLat(coordinates)
-                .setHTML(description)
-                .addTo(prevState.map);
-            });
-
-            prevState.map.on("mouseleave", "places", function () {
-              prevState.map.getCanvas().style.cursor = "";
-              popup.remove();
-            });
-          }
-        });
       }
 
-      // this.state.data.map((data) => {
-      //   let el = document.createElement("div");
-      //   el.className = "marker";
+      let dataSources = {
+        type: "geojson",
+        data: {
+          type: "FeatureCollection",
+          features: [],
+        },
+      };
 
-      //   let longitude = JSON.parse(data.long);
-      //   let latitude = JSON.parse(data.lat);
-      //   return new mapboxgl.Marker(el)
-      //     .setLngLat([longitude, latitude])
-      //     .addTo(this.state.map);
-      // });
+      let dataFeatures = [];
+      console.log(this.state.data);
+      this.state.data.map((data) =>
+        dataFeatures.push({
+          type: "Feature",
+          properties: {
+            description: `<strong>Citation: ${data.violation}</strong><p>IssueDate: ${data.issuedate}</p>"`,
+            icon: "theatre",
+          },
+          geometry: {
+            type: "Point",
+            coordinates: [JSON.parse(data.long), JSON.parse(data.lat)],
+          },
+        })
+      );
+
+      console.log(dataFeatures);
+      dataSources.data.features = dataFeatures;
+
+      this.state.map.once("render", () => {
+        if (!this.state.map.getSource("places") && this.state.zoom > 16) {
+          this.state.map.addSource("places", dataSources);
+
+          // Add a layer showing the places.
+          this.state.map.addLayer({
+            id: "places",
+            type: "symbol",
+            source: "places",
+            layout: {
+              "icon-image": "{icon}-15",
+              "icon-allow-overlap": true,
+            },
+          });
+        }
+
+        // Create a popup, but don't add it to the map yet.
+        var popup = new mapboxgl.Popup({
+          closeButton: false,
+          closeOnClick: false,
+        });
+
+        this.state.map.on("mouseenter", "places", (e) => {
+          // Change the cursor style as a UI indicator.
+          this.state.map.getCanvas().style.cursor = "pointer";
+
+          var coordinates = e.features[0].geometry.coordinates.slice();
+          var description = e.features[0].properties.description;
+
+          // Ensure that if the map is zoomed out such that multiple
+          // copies of the feature are visible, the popup appears
+          // over the copy being pointed to.
+          while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+          }
+
+          // Populate the popup and set its coordinates
+          // based on the feature found.
+          popup
+            .setLngLat(coordinates)
+            .setHTML(description)
+            .addTo(this.state.map);
+        });
+
+        this.state.map.on("mouseleave", "places", () => {
+          this.state.map.getCanvas().style.cursor = "";
+          popup.remove();
+        });
+      });
     }
-    // else if (this.state.zoom <= 16 && points !== 0) {
-    //   let points = document.getElementsByClassName("marker");
 
-    //   while (points.length > 0) points[0].remove();
-    // }
+    if (this.state.map.getSource("places") && this.state.zoom < 16) {
+      console.log("triggered");
+      this.state.map.removeLayer("places");
+      this.state.map.removeSource("places");
+
+      this.setState({
+        data: [],
+      });
+    }
   }
 
   render() {
