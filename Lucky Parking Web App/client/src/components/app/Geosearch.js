de500bb1d08c1b5ac7f0ab8e577fb087e05e1ce5
Derick Park <derickmpark@gmail.com>
2020-05-07 20:09:51 -0700

    [prog] Reformat web layout re-organize file structure

--- Lucky Parking Web App/client/src/components/app/Geosearch.js
+++ Lucky Parking Web App/client/src/components/app/Geosearch.js
@@ -2,7 +2,6 @@ import React from "react";
 import DatePicker from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
-import "./geosearch.css";
 
 class Geosearch extends React.Component {
   state = {
