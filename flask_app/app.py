8c1c6c0927480282d994454b128cf99e2775b3fd
Andy <andyvuhoang.data@gmail.com>
2020-06-11 12:41:55 -0700

    Change get_tileset() to connect to tilequery API

--- flask_app/app.py
+++ flask_app/app.py
@@ -12,17 +12,20 @@ app = Flask(
     __name__
 )
 
+@app.route('/')
+def hello_world():
+    return 'Hello world!'
 
-@app.route('/get_tileset/<string:tileset_id>/<int:zoom>/<int:x>/<int:y>.<string:format>')
-def get_tileset(tileset_id,zoom,x,y,format):
-    # tileset API documentation: https://docs.mapbox.com/api/maps/#tilesets
-    assert zoom <= 18 and zoom >= 0, "zoom must be in range [0, 18]"
-    assert x <= (zoom**2-1) and x >= 0, "x must be in range [0, zoom^2 - 1]"
-    assert y <= (zoom**2-1) and y >= 0, "y must be in range [0, zoom^2 - 1]"
-    assert format in ['mvt', 'vector.pbf'], "format must be mvt or vector.pbf"
+@app.route('/test')
+def test():
+    return 'test'
 
-    url = MAPBOX_BASE_PATH + '/v4/' + tileset_id + '/' + str(zoom) + '/' + str(x) + '/' + str(y) + '.' + format
-    url += '?access_token=' + MAPBOX_API_KEY
+@app.route('/get_tileset/<tileset_id>/<lat>,<lon>/<radius>.json')
+def get_tileset(tileset_id,lat,lon,radius):
+    # tileset API documentation: https://docs.mapbox.com/api/maps/#tilequery
+
+    url = MAPBOX_BASE_PATH + '/v4/' + tileset_id + '/tilequery/' + lon + ',' + lat + '.json?radius=' + radius
+    url += '&access_token=' + MAPBOX_API_KEY
     tileset = requests.get(url)
     
     return tileset.content
