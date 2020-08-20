d674e814720b0ebb4b4054489d0f51aec5c4ea90
sam <samreality4@gmail.com>
2020-08-20 16:59:00 -0700

    refractored code

---
+++ Lucky Parking Web App/client/src/components/app/map/MapLayers.js
@@ -0,0 +1,18 @@
+export const places = {
+    id: "places",
+    type: "symbol",
+    source: "places",
+    layout: {
+      "icon-image": "{icon}-15",
+      "icon-allow-overlap": true,
+    }
+};
+  export const meters = {
+    id: "meters",
+    source: "meters",
+    type: "line",
+    "source-layer": "meter_lines-1l60am",
+    paint: {
+      "line-color": "#e50cff",
+      "line-width": 2,
+    }}
