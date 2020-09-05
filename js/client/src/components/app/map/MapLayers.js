a7d9ad74aeb8d0f0466d4f665da75aa6f12c7f48
sam <samreality4@gmail.com>
2020-09-05 15:45:23 -0700

    changed map to dark style and changed bicycles to dots

--- js/client/src/components/app/map/MapLayers.js
+++ js/client/src/components/app/map/MapLayers.js
@@ -1,18 +1,23 @@
+import circle from "../../../assets/images/icons/circle-15.svg";
+
 export const places = {
-    id: "places",
-    type: "symbol",
-    source: "places",
-    layout: {
-      "icon-image": "{icon}-15",
-      "icon-allow-overlap": true,
-    }
+  id: "places",
+  type: "circle",
+  source: "places",
+  paint: {
+    "circle-radius": 4,
+    "circle-color": "#fddb3a",
+    'circle-stroke-width': 1,
+    "circle-opacity": 0.5,
+  },
 };
-  export const meters = {
-    id: "meters",
-    source: "meters",
-    type: "line",
-    "source-layer": "meter_lines-1l60am",
-    paint: {
-      "line-color": "#e50cff",
-      "line-width": 2,
-    }}
+export const meters = {
+  id: "meters",
+  source: "meters",
+  type: "line",
+  "source-layer": "meter_lines-1l60am",
+  paint: {
+    "line-color": "#e50cff",
+    "line-width": 2,
+  },
+};
