9c62f233d3ac8c500ce5d08b90f60afe2adb9d42
sam <samaudacity2@gmail.com>
2020-09-16 17:00:30 -0700

    modified controller for test

--- js/server/controllers.js
+++ js/server/controllers.js
@@ -6,7 +6,7 @@ module.exports = {
 
     dbHelpers
       .query(
-        `SELECT * FROM citations WHERE longitude BETWEEN ${longitude} - 0.02 AND ${longitude} + 0.02 AND latitude BETWEEN ${latitude} - 0.005 AND ${latitude} + 0.005`
+        `SELECT * FROM test2`
       )
       .then((data) => {
         res.status(200).send(data.rows);
