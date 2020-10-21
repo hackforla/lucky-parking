13e6158ba919f44d41fa5aa0e72c02fe07348e6d
Greg Pawin <36276149+gregpawin@users.noreply.github.com>
2020-10-20 17:29:27 -0700

    Update controllers.js

--- js/server/controllers.js
+++ js/server/controllers.js
@@ -1,12 +1,12 @@
 dbHelpers = require("../database/index.js");
 module.exports = {
   getAll: (req, res) => {
-    let longitude = JSON.parse(req.query.longitude);
-    let latitude = JSON.parse(req.query.latitude);
+    let longitude = JSON.parse(req.query.Longitude);
+    let latitude = JSON.parse(req.query.Latitude);
 
     dbHelpers
       .query(
-        `SELECT * FROM test2`
+        `SELECT * FROM citations`
       )
       .then((data) => {
         res.status(200).send(data.rows);
