292120de993dd8d9e8341204a7544070732610c6
gregpawin <gregpawin@gmail.com>
2020-11-03 14:12:56 -0800

    Updated preprocess code

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python
 """preprocess.py: Cleaning and preprocessing of raw parking citation data"""
-__author__      = "Hack for LA - Lucky Parking Project"
-
 
 import numpy as np
 import pandas as pd
-import datetime as dt
-import math
-import requests
-import seaborn as sns
-import sys
 
 # Function to prepare the time values so that they can be parsed
 def time_padding(time):
@@ -17,18 +10,19 @@ def time_padding(time):
   time = str(int(time))
   return '0'*(4-len(time)) + time
 
-df['Issue time'].fillna(0, inplace=True)
+def dataset_cleaning(df):
 
-# Apply padding to Issue_time
-df['Issue time'] = df['Issue time'].apply(time_padding)
-df['Datetime'] = pd.to_datetime(df['Issue Date'] + ' ' + df['Issue time'], 
-                                format='%m/%d/%Y %H%M')
-df = df.drop(['Issue Date', 'Issue time'], axis=1)
-mask = (df['Datetime'].dt.year > 2008) & (df['Datetime'].dt.year <= 2020)
+	df['issue_time'].fillna(0, inplace=True)
 
-df = df.loc[mask].reset_index(drop=True)
-df['Plate Expiry Date Parsed'] = pd.to_datetime(df['Plate Expiry Date'],format='%Y%m', errors='coerce')
-df.loc[df['Plate Expiry Date Parsed'].dt.year > 2021, 'Plate Expiry Date Parsed'] = np.nan
-
-makes_lemmas = pd.read_csv('make.csv', delimiter='\t')
-makes_lemmas['alias'] = makes_lemmas.alias.apply(lambda x: x.split(','))
+	# Apply padding to Issue_time
+	df['issue_time'] = df['issue_time'].apply(time_padding)
+	df['datetime'] = pd.to_datetime(df['issue_date'] + ' ' + df['issue_time'], 
+		                        format='%m/%d/%Y %H%M')
+	df = df.drop(['issue_date', 'issue_time', 'plate_expiry_date', 'VIN'], axis=1)
+	mask = (df['datetime'].dt.year > 2008) & (df['datetime'].dt.year <= 2020)
+	df = df.loc[mask].reset_index(drop=True)
+	makes_lemmas = pd.read_csv('make.csv', delimiter='\t')
+	makes_lemmas['alias'] = makes_lemmas.alias.apply(lambda x: x.split(','))
+	for ind, data in makes_lemmas.iterrows():
+		df.make.replace(data['alias'], data['make']
+return df
