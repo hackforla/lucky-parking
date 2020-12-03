1348ae5f77ce83d81572ef3e0567fd268e77c73e
Matthew Ginther <42285261+mg78856@users.noreply.github.com>
2020-12-03 13:02:10 -0800

    added query/current progress

--- js/client/src/components/app/Geosearch.js
+++ js/client/src/components/app/Geosearch.js
@@ -6,6 +6,9 @@ import { connect } from "react-redux";
 
 import "react-datepicker/dist/react-datepicker.css";
 
+const axios = require("axios");
+const API_URL = process.env.REACT_APP_API_URL;
+
 mapboxgl.accessToken = process.env.REACT_APP_MAP_BOX_TOKEN;
 
 const mapStateToProps = (state) => {
@@ -25,6 +28,26 @@ const ConnectGeosearch = ({ map, getMap }) => {
 
   const searchContainer = useRef();
 
+  function fetchTimeData() {
+    axios
+      .get(`${API_URL}/api/timestamp`, {
+        params: {
+          startDate: startDate,
+          endDate: endDate,
+        },
+      })
+      .then((data) => {
+        console.log('Time: ' + data.data)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  useEffect(() => {
+    fetchTimeData();
+  }, [startDate, endDate])
+
   getMap(searchContainer);
 
   return (
@@ -35,122 +58,21 @@ const ConnectGeosearch = ({ map, getMap }) => {
           <label>From :</label>
           <DatePicker
             selected={startDate}
-            onChange={(e) => {
-              setStartDate(e);
-            }}
+            showTimeSelect
+            timeFormat="HH:mm"
+            dateFormat="yyyy/MM/dd HH:mm:ss"
+            onChange={date => setStartDate(date)}
           />
-          <select
-            className="geosearch__main-time-select"
-            style={{ marginRight: "5rem" }}
-          >
-            <option>12:00 AM</option>
-            <option>12:30 AM</option>
-            <option>1:00 AM</option>
-            <option>1:30 AM</option>
-            <option>2:00 AM</option>
-            <option>2:30 AM</option>
-            <option>3:00 AM</option>
-            <option>3:30 AM</option>
-            <option>4:00 AM</option>
-            <option>4:30 AM</option>
-            <option>5:00 AM</option>
-            <option>5:30 AM</option>
-            <option>6:00 AM</option>
-            <option>6:30 AM</option>
-            <option>7:00 AM</option>
-            <option>7:30 AM</option>
-            <option>8:00 AM</option>
-            <option>8:30 AM</option>
-            <option>9:00 AM</option>
-            <option>9:30 AM</option>
-            <option>10:00 AM</option>
-            <option>10:30 AM</option>
-            <option>11:00 AM</option>
-            <option>11:30 AM</option>
-            <option>12:00 PM</option>
-            <option>12:30 PM</option>
-            <option>1:00 PM</option>
-            <option>1:30 PM</option>
-            <option>2:00 PM</option>
-            <option>2:30 PM</option>
-            <option>3:00 PM</option>
-            <option>3:30 PM</option>
-            <option>4:00 PM</option>
-            <option>4:30 PM</option>
-            <option>5:00 PM</option>
-            <option>5:30 PM</option>
-            <option>6:00 PM</option>
-            <option>6:30 PM</option>
-            <option>7:00 PM</option>
-            <option>7:30 PM</option>
-            <option>8:00 PM</option>
-            <option>8:30 PM</option>
-            <option>9:00 PM</option>
-            <option>9:30 PM</option>
-            <option>10:00 PM</option>
-            <option>10:30 PM</option>
-            <option>11:00 PM</option>
-            <option>11:30 PM</option>
-          </select>
         </div>
         <div className="geoSearchBarComponents">
           <label>To :</label>
           <DatePicker
             selected={endDate}
-            onChange={(e) => {
-              setEndDate(e);
-            }}
+            showTimeSelect
+            timeFormat="HH:mm"
+            dateFormat="yyyy/MM/dd HH:mm:ss"
+            onChange={date => setEndDate(date)}
           />
-          <select className="geosearch__main-time-select">
-            <option>12:00 AM</option>
-            <option>12:30 AM</option>
-            <option>1:00 AM</option>
-            <option>1:30 AM</option>
-            <option>2:00 AM</option>
-            <option>2:30 AM</option>
-            <option>3:00 AM</option>
-            <option>3:30 AM</option>
-            <option>4:00 AM</option>
-            <option>4:30 AM</option>
-            <option>5:00 AM</option>
-            <option>5:30 AM</option>
-            <option>6:00 AM</option>
-            <option>6:30 AM</option>
-            <option>7:00 AM</option>
-            <option>7:30 AM</option>
-            <option>8:00 AM</option>
-            <option>8:30 AM</option>
-            <option>9:00 AM</option>
-            <option>9:30 AM</option>
-            <option>10:00 AM</option>
-            <option>10:30 AM</option>
-            <option>11:00 AM</option>
-            <option>11:30 AM</option>
-            <option>12:00 PM</option>
-            <option>12:30 PM</option>
-            <option>1:00 PM</option>
-            <option>1:30 PM</option>
-            <option>2:00 PM</option>
-            <option>2:30 PM</option>
-            <option>3:00 PM</option>
-            <option>3:30 PM</option>
-            <option>4:00 PM</option>
-            <option>4:30 PM</option>
-            <option>5:00 PM</option>
-            <option>5:30 PM</option>
-            <option>6:00 PM</option>
-            <option>6:30 PM</option>
-            <option>7:00 PM</option>
-            <option>7:30 PM</option>
-            <option>8:00 PM</option>
-            <option>8:30 PM</option>
-            <option>9:00 PM</option>
-            <option>9:30 PM</option>
-            <option>10:00 PM</option>
-            <option>10:30 PM</option>
-            <option>11:00 PM</option>
-            <option>11:30 PM</option>
-          </select>
         </div>
       </div>
     </div>
