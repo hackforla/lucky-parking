aff70b00cf22a8578861cd058b50e6f90585209d
Matthew Ginther <42285261+mg78856@users.noreply.github.com>
2020-12-03 13:09:02 -0800

    added query/current progress

--- js/server/router.js
+++ js/server/router.js
@@ -2,5 +2,7 @@ const router = require("express").Router();
 const controllers = require("./controllers.js");
 
 router.route("/citation").get(controllers.getAll);
+router.route("/timestamp").get(controllers.getTimestamps);
+
 
 module.exports = router;
