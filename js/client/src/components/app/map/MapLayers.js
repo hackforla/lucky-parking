ab95bb182a9ccbe00acd8c06452d1b941c02dd92
JacobZwang <59858341+JacobZwang@users.noreply.github.com>
2020-12-14 19:05:47 -0800

    create heatmap layer

--- js/client/src/components/app/map/MapLayers.js
+++ js/client/src/components/app/map/MapLayers.js
@@ -2,11 +2,74 @@ export const places = {
   id: "places",
   type: "circle",
   source: "places",
-  "source-layer": 'Lucky_Citations-d050ms',
+  "source-layer": "Lucky_Citations-d050ms",
   paint: {
     "circle-radius": 4,
     "circle-color": "#fddb3a",
-    'circle-stroke-width': 1,
-    "circle-opacity": 0.5,
+    "circle-stroke-width": 0,
+    "circle-opacity": {
+      stops: [
+        [0, 0],
+        [14, 0.0],
+        [18, 0.01],
+        [20, 1],
+      ],
+    },
+  },
+};
+
+export const heatmap = {
+  id: "heatmap",
+  type: "heatmap",
+  source: "places",
+  "source-layer": "Lucky_Citations-d050ms",
+  paint: {
+    "heatmap-radius": {
+      stops: [
+        [0, 100],
+        [8, 10],
+        [10, 10],
+        [15, 30],
+      ],
+    },
+
+    "heatmap-intensity": {
+      stops: [
+        [0, 0.001],
+        [8, 0.001],
+        [13, 0.005],
+        [14, 0.01],
+        [15, 0.01],
+      ],
+    },
+
+    "heatmap-opacity": {
+      stops: [
+        [0, 1],
+        [10, 1],
+        [14, 1],
+        [15, 0.9],
+      ],
+    },
+
+    "heatmap-color": [
+      "interpolate",
+      ["linear"],
+      ["heatmap-density"],
+      0,
+      "rgba(3, 252, 90, 0)",
+      0.00999,
+      "rgba(3, 252, 90, 0)",
+      0.01,
+      "rgba(252, 219, 3, 0.05)",
+      0.09999,
+      "rgba(252, 219, 3, 0.05)",
+      0.1,
+      "rgba(255, 102, 0, 0.35)",
+      0.9,
+      "rgba(255, 102, 0, 0.35)",
+      1,
+      "rgba(178,24,43, 0.75)",
+    ],
   },
 };
