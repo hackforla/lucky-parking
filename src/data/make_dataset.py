31e6aa3c2355e8f71007b450ff33eda2340a2862
gregpawin <gregpawin@gmail.com>
2020-12-27 14:35:47 -0800

    fixed index and sep to '|' to be more db friendly

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import click
+
 # import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv, set_key
@@ -13,55 +14,59 @@ from pyproj import Transformer
 from typing import Union
 
 
-#Load project directory
+# Load project directory
 PROJECT_DIR = Path(__file__).resolve().parents[2]
 
+
 @click.command()
-@click.argument('input_filedir', type=click.Path(exists=True))
-@click.argument('output_filedir', type=click.Path())
+@click.argument("input_filedir", type=click.Path(exists=True))
+@click.argument("output_filedir", type=click.Path())
 def main(input_filedir: str, output_filedir: str):
-    """ Downloads full dataset from lacity.org, and runs data processing
-        scripts to turn raw data into cleaned data ready
-        to be analyzed. Also updates environmental
-        variable RAW_DATA_FILEPATH.
+    """Downloads full dataset from lacity.org, and runs data processing
+    scripts to turn raw data into cleaned data ready
+    to be analyzed. Also updates environmental
+    variable RAW_DATA_FILEPATH.
     """
     # If run as main, data is downloaded,  10% sampled, and cleaned
     # automatically
-    clean(create_sample(download_raw(input_filedir), 'data/interim', 0.1),
-          output_filedir)
+    clean(
+        create_sample(download_raw(input_filedir), "data/interim", 0.1), output_filedir
+    )
 
 
 def download_raw(input_filedir: str) -> Path:
-    """ Downloads raw dataset from lacity.org to input_filedir as {date}
+    """Downloads raw dataset from lacity.org to input_filedir as {date}
     raw.csv. Also updates environmental variable RAW_DATA_FILEPATH.
     """
     # Create name string using download date
     date_string = date.today().strftime("%Y-%m-%d")
 
-    print('This will take a few minutes')
+    print("This will take a few minutes")
 
     # Setup connection and download into raw data folder
     http = urllib3.PoolManager()
-    url = 'https://data.lacity.org/api/views/' + \
-        'wjz9-h9np/rows.csv?accessType=DOWNLOAD'
-    RAW_DATA_FILEPATH = PROJECT_DIR / input_filedir / \
-        (date_string + '_raw.csv')
-    with http.request('GET', url, preload_content=False) as res,\
-            open(RAW_DATA_FILEPATH, 'wb') as out_file:
+    url = (
+        "https://data.lacity.org/api/views/" + "wjz9-h9np/rows.csv?accessType=DOWNLOAD"
+    )
+    RAW_DATA_FILEPATH = PROJECT_DIR / input_filedir / (date_string + "_raw.csv")
+    with http.request("GET", url, preload_content=False) as res, open(
+        RAW_DATA_FILEPATH, "wb"
+    ) as out_file:
         shutil.copyfileobj(res, out_file)
 
-    print('Finished downloading raw dataset')
+    print("Finished downloading raw dataset")
 
     # Save raw file path as RAW_DATA_FILEPATH into .env
-    set_key(find_dotenv(), 'RAW_DATA_FILEPATH', str(RAW_DATA_FILEPATH))
+    set_key(find_dotenv(), "RAW_DATA_FILEPATH", str(RAW_DATA_FILEPATH))
     return RAW_DATA_FILEPATH
 
 
-def create_sample(target_file: Union[Path, str], output_filedir: str,
-                  sample_frac: float) -> Path:
-    ''' Samples the raw dataset to create a smaller dataset via random
+def create_sample(
+    target_file: Union[Path, str], output_filedir: str, sample_frac: float
+) -> Path:
+    """Samples the raw dataset to create a smaller dataset via random
     sampling according to sample_frac.
-    '''
+    """
     # Change str filepath into Path
     if isinstance(target_file, str):
         target_file = Path(target_file)
@@ -71,108 +76,163 @@ def create_sample(target_file: Union[Path, str], output_filedir: str,
 
     # Create filename with sample fraction appended to the name
     # 0.1 turns into 01, 0.25 turns into 025, etc
-    SAMPLE_FILEPATH = PROJECT_DIR / output_filedir / (target_file.stem +
-        '_' + str(sample_frac).replace('.', '') + 'samp.csv')
+    SAMPLE_FILEPATH = (
+        PROJECT_DIR
+        / output_filedir
+        / (target_file.stem + "_" + str(sample_frac).replace(".", "") + "samp.csv")
+    )
 
-    print('Creating sample')
+    print("Creating sample")
 
     # Read raw data and skiprows using random.random()
     pd.read_csv(
-            target_file,
-            header=0,
-            index_col=0,
-            skiprows=lambda i: i > 0 and random.random() > sample_frac,
-            low_memory=False
-        ).to_csv(SAMPLE_FILEPATH)
+        target_file,
+        header=0,
+        index_col=0,
+        skiprows=lambda i: i > 0 and random.random() > sample_frac,
+        low_memory=False,
+    ).reset_index(drop=True).to_csv(SAMPLE_FILEPATH, index=False)
 
-    print('Sample complete')
+    print("Sample complete")
 
     return SAMPLE_FILEPATH
 
 
 def clean(target_file: Union[Path, str], output_filedir: str):
-    ''' Removes unnecessary columns, erroneous data points and aliases,
+    """Removes unnecessary columns, erroneous data points and aliases,
     changes geometry projection from epsg:2229 to epsg:4326, and converts
     time to datetime type.
-    '''
+    """
     # Change str filepath into Path
     if isinstance(target_file, str):
         target_file = Path(target_file)
 
-    print('Cleaning dataset')
+    print("Cleaning dataset")
 
     # Read file into dataframe
     df = pd.read_csv(target_file, low_memory=False)
 
     # Select columns of interest
-    df = df[['Issue Date', 'Issue time', 'RP State Plate', 'Make',
-             'Body Style', 'Color', 'Location', 'Violation code',
-             'Violation Description', 'Fine amount', 'Latitude', 'Longitude']]
+    df = df[
+        [
+            "Issue Date",
+            "Issue time",
+            "RP State Plate",
+            "Make",
+            "Body Style",
+            "Color",
+            "Location",
+            "Violation code",
+            "Violation Description",
+            "Fine amount",
+            "Latitude",
+            "Longitude",
+        ]
+    ]
 
     # Filter out data points with bad coordinates
     df = df[(df.Latitude != 99999) & (df.Longitude != 99999)]
 
     # Filter out data points with no time/date stamps
-    df = df[(df['Issue Date'].notna()) & (df['Issue time'].notna())]
+    df = df[
+        (df["Issue Date"].notna())
+        & (df["Issue time"].notna())
+        & (df["Fine amount"].notna())
+    ]
 
-    # Convert Issue time and Issue Date strings into a combined datetime type 
-    df['Issue time'] = df['Issue time'].apply(lambda x: '0'*(4-len(str(int(x)))) + str(int(x)))
-    df['Datetime'] = pd.to_datetime(df['Issue Date'] + ' ' + df['Issue time'],
-        format='%m/%d/%Y %H%M')
+    # Convert Issue time and Issue Date strings into a combined datetime type
+    df["Issue time"] = df["Issue time"].apply(
+        lambda x: "0" * (4 - len(str(int(x)))) + str(int(x))
+    )
+    df["Datetime"] = pd.to_datetime(
+        df["Issue Date"] + " " + df["Issue time"], format="%m/%d/%Y %H%M"
+    )
 
     # Drop original date/time columns
-    df = df.drop(['Issue Date', 'Issue time'], axis=1)
+    df = df.drop(["Issue Date", "Issue time"], axis=1)
 
     # Make column names more coding friendly except for Lat/Lon
-    df.columns = ['state_plate', 'make', 'body_style', 'color', 'location', 'violation_code',
-    'violation_description', 'fine_amount', 'Latitude', 'Longitude', 'datetime']
+    df.columns = [
+        "state_plate",
+        "make",
+        "body_style",
+        "color",
+        "location",
+        "violation_code",
+        "violation_description",
+        "fine_amount",
+        "Latitude",
+        "Longitude",
+        "datetime",
+    ]
 
     # Read in make aliases
-    make_df = pd.read_csv(PROJECT_DIR / 'references/make.csv', delimiter='\t')
-    make_df['alias'] = make_df.alias.apply(lambda x: x.split(','))
+    make_df = pd.read_csv(PROJECT_DIR / "references/make.csv", delimiter="\t")
+    make_df["alias"] = make_df.alias.apply(lambda x: x.split(","))
 
     # Iterate over makes and replace aliases
     for _, data in make_df.iterrows():
-        df = df.replace(data['alias'], data['make'])
+        df = df.replace(data["alias"], data["make"])
 
     # Instantiate projection converter and change projection
-    transformer = Transformer.from_crs('epsg:2229', 'epsg:4326')
-    df['latitude'], df['longitude'] = transformer.transform(df['Latitude'].values, df['Longitude'].values)
-    
+    transformer = Transformer.from_crs("epsg:2229", "epsg:4326")
+    df["latitude"], df["longitude"] = transformer.transform(
+        df["Latitude"].values, df["Longitude"].values
+    )
+
     # Drop original coordinate columns
-    df = df.drop(['Latitude', 'Longitude'], axis=1)
+    df = df.drop(["Latitude", "Longitude"], axis=1)
     # df.reset_index(drop=True,inplace=True)
     # df.drop('Ticket number', axis=1)
 
     # Extract weekday and add as column
-    df['weekday'] = df.datetime.dt.weekday.astype(str).replace({'0': 'Monday',
-        '1': 'Tuesday', '2': 'Wednesday', '3': 'Thursday', '4': 'Friday', '5': 'Saturday',
-        '6': 'Sunday'})
+    df["weekday"] = df.datetime.dt.weekday.astype(str).replace(
+        {
+            "0": "Monday",
+            "1": "Tuesday",
+            "2": "Wednesday",
+            "3": "Thursday",
+            "4": "Friday",
+            "5": "Saturday",
+            "6": "Sunday",
+        }
+    )
 
-    # Reset index
+    # Set fine amount as int
+    df["fine_amount"] = df.fine_amount.astype(int)
+
+    # Drop filtered index and add new one
     df.reset_index(drop=True, inplace=True)
+    df.reset_index(inplace=True)
 
     # Save as csv to output_filedir annotated as processed
-    df.to_csv(PROJECT_DIR / output_filedir / (target_file.stem.replace('_raw', '_processed') + '.csv'))
+    df.to_csv(
+        PROJECT_DIR
+        / output_filedir
+        / (target_file.stem.replace("_raw", "_processed") + ".csv"),
+        index=False,
+        na_rep="",
+        sep="|",
+    )
+    print("Finished!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     # logging.basicConfig(level=logging.INFO, format=log_fmt)
-    
 
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
     load_dotenv(find_dotenv())
 
     # Create data folders
-    data_folders = ['raw', 'interim', 'external', 'processed']
-    if not os.path.exists(PROJECT_DIR / 'data'):
-        os.makedirs(PROJECT_DIR / 'data')
+    data_folders = ["raw", "interim", "external", "processed"]
+    if not os.path.exists(PROJECT_DIR / "data"):
+        os.makedirs(PROJECT_DIR / "data")
     for _ in data_folders:
-        if not os.path.exists(PROJECT_DIR / 'data' / _):
-            os.makedirs(PROJECT_DIR / 'data' / _)
-            with open(PROJECT_DIR / 'data' / _ / ".gitkeep", 'w'):
+        if not os.path.exists(PROJECT_DIR / "data" / _):
+            os.makedirs(PROJECT_DIR / "data" / _)
+            with open(PROJECT_DIR / "data" / _ / ".gitkeep", "w"):
                 pass
 
     # Run main function
