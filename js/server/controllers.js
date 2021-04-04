10d5faa9aaad7be748a677ef0c1386d67146a5fc
gregpawin <gregpawin@gmail.com>
2021-04-04 10:38:39 -0700

    fixed vulnerability and added keyed make test

--- js/server/controllers.js
+++ js/server/controllers.js
@@ -1,6 +1,6 @@
 dbHelpers = require("../database/index.js");
 
-function generateGeoData(data){
+function generateGeoData(data) {
   let dataSources = {
     type: "FeatureCollection",
     features: [],
@@ -21,7 +21,21 @@ function generateGeoData(data){
   return dataSources;
 }
 
+/*
+Dictionary for keyed values "make_ind"
+{'Mercury': 37, 'Alfa Romero': 50, 'Lamborghini': 64, 'Maserati': 43, 'Oldsmobile': 42, 'Yamaha': 53,
+'Chevrolet': 4, 'Porsche': 26, 'Checker': 60, 'Other': 15, 'International': 39, 'Volkswagen': 7,
+'Hino': 48, 'Suzuki': 40, 'Smart': 46, 'Dodge': 9, 'Geo Metro': 55, 'Merkur': 67, 'CIMC': 69,
+'Infinity': 17, 'Pontiac': 33, 'Rolls-Royce': 57, 'Isuzu': 38, 'Cadillac': 24, 'Honda': 1, 'Hyundai': 8,
+'Ford': 2, 'Mazda': 14, 'GMC': 16, 'Datsun': 61, 'Saab': 41, 'Grumman': 34, 'BMW': 5, 'Unknown': 27,
+'Kia': 11, 'Mitsubishi': 23, 'Mack': 68, 'Sterling': 63, 'Land Rover': 22, 'Fiat': 35, 'Hummer': 51,
+'Lincoln': 31, 'Acura': 20, 'Jaguar': 36, 'Freightliner': 29, 'Jeep': 12, 'Aston Martin': 65,
+'Nissan': 3, 'Harley-Davidson': 49, 'Toyota': 0, 'Volvo': 21, 'Winnebago': 56, 'Mercedes Benz': 6,
+'Kawasaki': 54, 'Chrysler': 18, 'Peterbuilt': 44, 'Triumph': 59, 'Scion': 58, 'Mini': 25, 'Saturn': 32,
+'Bentley': 52, 'Tesla': 30, 'Plymouth': 47, 'Daewoo': 66, 'Subaru': 19, 'MISC.': 70, 'Buick': 28,
+'Ferrari': 62, 'Lexus': 10, 'Kenworth': 45, 'Audi': 13}
 
+*/
 
 module.exports = {
   getAll: (req, res) => {
@@ -31,7 +45,7 @@ module.exports = {
 
     dbHelpers
       .query(
-        `SELECT index, ST_AsGeoJSON(geometry) FROM citations WHERE ST_X(geometry) BETWEEN ${longitude[0]} AND ${latitude[0]} AND ST_Y(geometry) BETWEEN ${longitude[1]} AND ${latitude[1]} LIMIT 15000`
+        `SELECT index, ST_AsGeoJSON(geometry) FROM test1 WHERE ST_X(geometry) BETWEEN ${longitude[0]} AND ${latitude[0]} AND ST_Y(geometry) BETWEEN ${longitude[1]} AND ${latitude[1]} LIMIT 15000`
       )
       .then((data) => {
         res.status(200).send(generateGeoData(data.rows));
@@ -39,14 +53,14 @@ module.exports = {
       .catch((err) => {
         res.status(404).send(err);
       });
-    
+
   },
   getPointData: (req, res) => {
     let index = req.query.index;
 
     dbHelpers
       .query(
-        `SELECT * FROM citations WHERE INDEX = '${index}'`
+        `SELECT * FROM test1 WHERE INDEX = '${index}'`
       )
       .then((data) => {
         res.status(200).send(data.rows);
@@ -63,7 +77,7 @@ module.exports = {
 
     dbHelpers
       .query(
-        `SELECT index, ST_AsGeoJSON(geometry) FROM citations WHERE ST_X(geometry) BETWEEN ${longitude[0]} AND ${latitude[0]} AND ST_Y(geometry) BETWEEN ${longitude[1]} AND ${latitude[1]} AND datetime BETWEEN '${startDate}' AND '${endDate}' LIMIT 15000`
+        `SELECT index, ST_AsGeoJSON(geometry) FROM test1 WHERE ST_X(geometry) BETWEEN ${longitude[0]} AND ${latitude[0]} AND ST_Y(geometry) BETWEEN ${longitude[1]} AND ${latitude[1]} AND datetime BETWEEN '${startDate}' AND '${endDate}' LIMIT 15000`
       )
       .then((data) => {
         res.status(200).send(generateGeoData(data.rows));
@@ -74,15 +88,15 @@ module.exports = {
   },
   drawSelect: (req, res) => {
     let polygon = req.query.polygon;
-    
+
 
     dbHelpers
       .query(
-        `SELECT ST_AsGeoJSON(geometry) FROM citations WHERE ST_Contains(ST_GeomFromGeoJSON('{
+        `SELECT ST_AsGeoJSON(geometry) FROM test1 WHERE ST_Contains(ST_GeomFromGeoJSON('{
           "type":"Polygon",
           "coordinates": [${polygon}],
           "crs": {"type": "name", "properties": {"name": "EPSG:4326"}}										                            
-        }'), citations.geometry) LIMIT 30000;`
+        }'), test1.geometry) LIMIT 30000;`
       )
       .then((data) => {
         res.status(200).send(generateGeoData(data.rows));
@@ -94,15 +108,15 @@ module.exports = {
   graph: (req, res) => {
     let polygon = req.query.polygon;
     let filterBy = req.query.filterBy;
-    
+
 
     dbHelpers
       .query(
-        `SELECT ${filterBy} AS "name", (COUNT(*) / (SUM(COUNT(*)) OVER() )) * 100 AS "y" FROM citations WHERE ST_Contains(ST_GeomFromGeoJSON('{
+        `SELECT ${filterBy} AS "name", (COUNT(*) / (SUM(COUNT(*)) OVER() )) * 100 AS "y" FROM test1 WHERE ST_Contains(ST_GeomFromGeoJSON('{
           "type":"Polygon",
           "coordinates": [${polygon}],
           "crs": {"type": "name", "properties": {"name": "EPSG:4326"}}										                            
-        }'), citations.geometry) GROUP BY ${filterBy};`
+        }'), test1.geometry) GROUP BY ${filterBy};`
       )
       .then((data) => {
         res.status(200).send(data.rows);
