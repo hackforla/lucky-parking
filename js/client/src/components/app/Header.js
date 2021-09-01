bde054805df52ad85c63c8c883e153740ddf1b9e
qiqicodes <qiqizheng0127@gmail.com>
2021-08-31 23:29:29 -0700

    [289-header-instruction] instructions component experimental. 1st stage: span header vw

--- js/client/src/components/app/Header.js
+++ js/client/src/components/app/Header.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React from "react";
 
 function Header() {
   return (
     <div className="header-container">
-      <div className="picture"/>
+      <div className="picture" />
+      <div>
+        <h1>LOS ANGELES PARKING CITATION DATA</h1>
+        <p>
+          Helping L.A. communities make informed decisions about parking
+          policies
+        </p>
+      </div>
     </div>
   );
 }
 
 export default Header;
-  
