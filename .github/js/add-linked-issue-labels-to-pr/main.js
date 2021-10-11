668aae8cce4e4a683f9c5b40da8368b8f9096b80
macho-catt <cacho.matthew@gmail.com>
2021-10-10 18:01:14 -0700

    Console.log

--- .github/js/add-linked-issue-labels-to-pr/main.js
+++ .github/js/add-linked-issue-labels-to-pr/main.js
@@ -8,8 +8,12 @@ const findLinkedIssue = require('../utils/find-linked-issue.js')
 function main({g, c}) {
   github = g
   context = c
-  console.log(github.event)
-  console.log(github.event.pull_request)
+  console.log('event:', github.event)
+  // figure out json tree for either github or context go get PR number
+  // otherwise pull it from the env outside the js file
+
+
+  // console.log(github.event.pull_request)
   // const body = github.event.pull_request.body 
   // const prNumber = github.event.number
 
