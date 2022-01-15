c64541cfb9205922597e8c2f5ed96590f27138ea
macho-catt <cacho.matthew@gmail.com>
2022-01-14 23:02:54 -0800

    Fix route

--- js/server/routes/router.js
+++ js/server/routes/router.js
@@ -7,7 +7,7 @@ router.get('/citation/graph', controller.graph);
 router.get('/citation/point', controller.getPointData);
 router.get('/timestamp', controller.getTimestamps);
 router.get('/zipcodes', controller.getZipLayer);
-router.get('citation/draw/zip', controller.zipSelect);
+router.get('/citation/draw/zip', controller.zipSelect);
 router.get('/citation/graph/zip', controller.zipGraph);
 
 module.exports = router;
