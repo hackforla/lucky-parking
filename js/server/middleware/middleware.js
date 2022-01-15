1b70f9b7124698927db6e4c4d3c67c92a70523d0
macho-catt <cacho.matthew@gmail.com>
2022-01-14 18:07:44 -0800

    Fix routes

--- js/server/middleware/middleware.js
+++ js/server/middleware/middleware.js
@@ -19,27 +19,6 @@ const errorHandler = (error, request, response, next) => {
   logger.error(error.message);
   response.status(404).send(error);
   
-  // if (error.name === 'CastError'){
-  //   return response.status(400).send({ 
-  //     error: 'malformatted id' 
-  //   });
-  // }
-  // else if (error.name === 'ValidationError'){
-  //   return response.status(400).json({ 
-  //     error: error.message 
-  //   });
-  // }
-  // else if (error.name === 'JsonWebTokenError'){
-  //   return response.status(401).json({
-  //     error: 'invalid token'
-  //   });
-  // }
-  // else if (error.name === 'TokenExpiredError'){
-  //   return response.status(401).json({
-  //     error: 'token expired'
-  //   });
-  // }
-
   next(error);
 }
 
--- js/server/routes/router.js
+++ js/server/routes/router.js
@@ -1,14 +1,13 @@
 const router = require('express').Router();
 const controller = require('../controllers/controllers');
 
-// router.route('/citation').get(controller.getAll);
 router.get('/citation', controller.getAll);
-router.route('/citation/draw').get(controller.drawSelect);
-router.route('/citation/graph').get(controller.graph);
-router.route('/citation/point').get(controller.getPointData);
-router.route('/timestamp').get(controller.getTimestamps);
-router.route('/zipcodes').get(controller.getZipLayer);
-router.route('/citation/draw/zip').get(controller.zipSelect);
-router.route('/citation/graph/zip').get(controller.zipGraph);
+router.get('/citation/draw', controller.drawSelect);
+router.get('/citation/graph', controller.graph);
+router.get('/citation/point', controller.getPointData);
+router.get('/timestamp', controller.getTimestamps);
+router.get('/zipcodes', controller.getZipLayer);
+router.get('citation/draw/zip', controller.zipSelect);
+router.get('/citation/graph/zip', controller.zipGraph);
 
 module.exports = router;
