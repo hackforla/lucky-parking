48b685aad6e0c30cd5efa4ddac50a0e002350ffb
Matthew Cacho <48004150+macho-catt@users.noreply.github.com>
2022-01-26 18:30:17 -0800

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -17,5 +17,3 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-// test comment
