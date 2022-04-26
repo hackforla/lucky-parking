85c2ef9859449598d39e265ea64403665bd11021
Diedrich Chavarria <diedrich.chavarria@yahoo.com>
2022-04-25 18:31:10 -0700

    modified functions drawSelect and zipSelect to query geo data specifying start date and end date

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -140,42 +140,19 @@ const getTimestamps = async (req, res) => {
   }
 };
 
-const drawSelect = async (req, res) => {
-  const { polygon } = req.query;
-  const query = format(
-    `
-    SELECT ST_AsGeoJSON(geometry) FROM test1
-    WHERE ST_Contains(ST_GeomFromGeoJSON('{
-      "type": "Polygon",
-      "coordinates": [%s],
-      "crs": {
-        "type": "name",
-        "properties": {
-          "name": "EPSG:4326"
-        }
-      }										                            
-    }'), test1.geometry)
-    LIMIT 30000;`,
-    polygon[0]
-  );
-
-  const data = await db.query(query);
-
-  if (data) {
-    res.status(200).send(generateGeoData(data.rows));
-  } else {
-    res.status(400).send({
-      data: 'No data',
-      success: false,
-    });
-  }
-};
-
 // Queries and send geo data based on dates user requested
-const drawSelectInDates = async (req, res) => {
+const drawSelect = async (req, res) => {
   const { polygon } = req.query;
   const { startDate } = req.query;
   const { endDate } = req.query;
+  let datesConditionStr = '';
+
+  if (
+    (startDate !== undefined || startDate !== null) &&
+    (endDate !== undefined || endDate !== null)
+  ) {
+    datesConditionStr = 'AND datetime BETWEEN %2L AND %3L';
+  }
 
   const query = format(
     `
@@ -196,7 +173,7 @@ const drawSelectInDates = async (req, res) => {
       ),
       test1.geometry
     )
-    AND datetime BETWEEN %2L AND %3L
+    ${datesConditionStr} 
     LIMIT 30000;`,
     polygon[0],
     startDate,
@@ -215,37 +192,19 @@ const drawSelectInDates = async (req, res) => {
   }
 };
 
-const zipSelect = async (req, res) => {
-  const zip = parseInt(req.query.zip, 10);
-  const query = format(
-    `
-    SELECT ST_AsGeoJSON(geometry)
-    FROM test1 AS citations
-    WHERE ST_WITHIN(citations.geometry, (
-      SELECT ST_SetSRID(the_geom, 4326)
-      FROM zipcodes
-      WHERE zip = %L
-    ));`,
-    zip
-  );
-
-  const data = await db.query(query);
-
-  if (data) {
-    res.status(200).send(generateGeoData(data.rows));
-  } else {
-    res.status(400).send({
-      data: 'No data',
-      success: false,
-    });
-  }
-};
-
 // Queries and sends coordinates based on a period of time directed by the user
-const zipSelectInDates = async (req, res) => {
+const zipSelect = async (req, res) => {
   const { zip } = req.query;
   const { startDate } = req.query;
   const { endDate } = req.query;
+  let datesConditionStr = ';';
+
+  if (
+    (startDate !== undefined || startDate !== null) &&
+    (endDate !== undefined || endDate !== null)
+  ) {
+    datesConditionStr = 'AND datetime BETWEEN %2L AND %3L;';
+  }
 
   const query = format(
     `
@@ -259,7 +218,7 @@ const zipSelectInDates = async (req, res) => {
           WHERE zip = %1L
         )
       )
-      AND datetime BETWEEN %2L AND %3L;
+      ${datesConditionStr}
     `,
     zip,
     startDate,
@@ -342,42 +301,6 @@ const zipGraph = async (req, res) => {
   }
 };
 
-// Queries and sends citations data filtered and based on dates that user requested
-const zipGraphInDates = async (req, res) => {
-  const { zip } = req.query;
-  const { filterBy } = req.query;
-  const { startDate } = req.query;
-  const { endDate } = req.query;
-
-  const query = format(
-    `
-    SELECT %1$L AS "name",
-    (COUNT(*) / (SUM(COUNT(*)) OVER() )) * 100 AS "y"
-    FROM test1 WHERE ST_Contains(
-      (SELECT ST_SetSRID(the_geom, 4326)
-      FROM zipcodes
-      WHERE zip = %2$L),
-    test1.geometry)
-    AND datetime BETWEEN %3L AND %4L
-    GROUP BY %1$s;`,
-    filterBy,
-    zip,
-    startDate,
-    endDate
-  );
-
-  const data = await db.query(query);
-
-  if (data) {
-    res.status(200).send(data.rows);
-  } else {
-    res.status(400).send({
-      data: 'No data',
-      success: false,
-    });
-  }
-};
-
 module.exports = {
   generateGeoData,
   generateZipData,
@@ -389,7 +312,4 @@ module.exports = {
   zipSelect,
   graph,
   zipGraph,
-  zipSelectInDates,
-  zipGraphInDates,
-  drawSelectInDates,
 };
