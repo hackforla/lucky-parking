79b1b837f3325fa07720d710d302437f6dbdde32
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2022-11-25 01:28:14 -0800

    refactor(legacy-api)!: migrate legacy api code
    Reintroduce Express API code as application in monorepo.
    
    BREAKING CHANGE: server code is found in `api` and not `server`

---
+++ apps/api-legacy/src/app/utils/interceptor.js
@@ -0,0 +1,18 @@
+module.exports = {
+  mockRequest: () => {
+    const req = {};
+    req.body = jest.fn().mockReturnValue(req);
+    req.params = jest.fn().mockReturnValue(req);
+    req.query = jest.fn().mockReturnValue(req);
+    return req;
+  },
+
+  mockResponse: () => {
+    const res = {};
+    res.send = jest.fn().mockReturnValue(res);
+    res.status = jest.fn().mockReturnValue(res);
+    res.json = jest.fn().mockReturnValue(res);
+    return res;
+  },
+  // mockNext: () => jest.fn()
+};
