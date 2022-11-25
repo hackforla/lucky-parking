cbca30da6b17cd6322776479abd575569ef1004c
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2022-11-24 17:52:09 -0800

    refactor!: remove codebase
    Remove everything from codebase.
    
    BREAKING CHANGE: all code removed

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const app = require('./app');
-const config = require('./config/config');
-const logger = require('./utils/logger');
-
-const server = http.createServer(app);
-server.listen(config.PORT, () => {
-  logger.info(`Listening on port ${config.PORT}`);
-});
