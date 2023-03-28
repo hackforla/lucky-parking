8baa2581930b5c420d7a51b5e62d0e1fe680360a
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2023-03-27 21:18:49 -0700

    refactor!: delete codebase
    BREAKING-CHANGE: Everything is deleted.

--- apps/api-legacy/src/index.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const app = require('./app/app');
-const config = require('./app/config/config');
-const logger = require('./app/utils/logger');
-
-const server = http.createServer(app);
-server.listen(config.PORT, () => {
-  logger.info(`Listening on port ${config.PORT}`);
-});
--- libs/ui/src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Tag } from './tag';
-
-// Layout Components
-export { default as FullContainer } from './layout/full-container';
-export { default as ScreenContainer } from './layout/screen-container';
