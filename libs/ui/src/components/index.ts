ab198d4148d07989fd4c93030b3dd94440a52d2a
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2023-04-10 17:38:40 -0700

    feat: migrate ui library

---
+++ libs/ui/src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Tag } from './tag';
+
+// Layout Components
+export { default as FullContainer } from './layout/full-container';
+export { default as ScreenContainer } from './layout/screen-container';
--- libs/ui/vite.config.ts
+++ libs/ui/vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "./src/main.ts"),
+      name: "UI",
+      fileName: "ui",
+    },
+    rollupOptions: {
+      external: ["react"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
   plugins: [react()],
-})
+});
