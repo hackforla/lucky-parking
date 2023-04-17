b54a6a58567de971d9b0f6b0012d8223ba84d438
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2023-04-16 20:57:25 -0700

    config: set color scheme in tailwind theme

--- libs/ui/tailwind.config.js
+++ libs/ui/tailwind.config.js
@@ -1,8 +1,49 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "lp-blue": {
+          100: "#EFF7FF",
+          200: "#D4E7FA",
+          300: "#A1CCF8",
+          400: "#4A89D2",
+          500: "#205CA2",
+          600: "#154782",
+          700: "#06356B",
+        },
+        "lp-violet": {
+          200: "#EBDBFB",
+          500: "#7063CD",
+          700: "#180C69",
+        },
+        "lp-green": {
+          200: "#B3ECEC",
+          500: "#29B0B0",
+          700: "#03433E",
+        },
+        dark: {
+          100: "#CFCFD1",
+          200: "#A5A5A6",
+          300: "#7A7A7B",
+          400: "#505050",
+          500: "#1F1F1F",
+        },
+        light: {
+          100: "#FBFBFB",
+          200: "#F2F2F4",
+          300: "#EDEDEF",
+          400: "rgba(229, 229, 231, 0.8)",
+          500: "#E5E5E7",
+        },
+      },
+      fontFamily: {
+        "sans": ["Work Sans", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
