c8655ac2ff93a45649637da2a4f4812111fc9665
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2023-05-12 02:39:16 -0700

    feat: create header component

---
+++ packages/website/src/widgets/header/header.tsx
@@ -0,0 +1,27 @@
+import { Button, ButtonVariant } from "@lucky-parking/ui";
+import Logo from "src/shared/ui/logo";
+import VerticalDivider from "@/shared/ui/vertical-divider";
+
+export default function Header() {
+  return (
+    <div className="flex flex-1 h-[60px] px-5 py-2 justify-between items-center">
+      {/* Branding */}
+      <div className="flex flex-1 items-center space-x-6">
+        <Logo />
+          
+        {/* Description */}
+        <div>
+          <p className="heading-4">Los Angeles Parking Citation Data</p>
+          <p className="paragraph-2 font-medium">
+            Helping L.A. make informed decisions about parking policies
+          </p>
+        </div>
+      </div>
+
+      {/* Actions */}
+      <div>
+        <Button variant={ButtonVariant.secondary}>HOW IT WORKS</Button>
+      </div>
+    </div>
+  );
+}
