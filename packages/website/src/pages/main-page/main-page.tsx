57ca3c2b19456a659f6c176cbefac09a8c161386
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2023-05-22 16:31:34 -0700

    feat(website): render action box widget in main page

--- packages/website/src/pages/main-page/main-page.tsx
+++ packages/website/src/pages/main-page/main-page.tsx
@@ -1,11 +1,17 @@
 import { Map } from "@/entities";
 import Header from "@/widgets/header";
 import Page from "@/shared/ui/page";
+import ActionBox from "src/widgets/action-box";
 
 function MainPage() {
   return (
     <Page>
       <Header />
+
+      <div className="absolute z-50 top-20 left-4">
+        <ActionBox />
+      </div>
+
       <Map />
     </Page>
   );
