bc143df03c47981a5ea67daabe220599c0a5bc22
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2023-05-31 18:16:56 -0700

    feat(ui): extend functionality of search input component

--- packages/ui/src/components/search-input/search-input.tsx
+++ packages/ui/src/components/search-input/search-input.tsx
@@ -1,11 +1,25 @@
+import _ from "lodash";
 import SearchIcon from "@mui/icons-material/Search";
+import { ChangeEvent, PropsWithChildren, useMemo, useState } from "react";
 
-interface SearchInputProps {
+interface SearchInputProps extends PropsWithChildren {
+  onChange?: (arg0: any) => void;
   placeholder?: string;
+  value: string;
 }
 
 export default function SearchInput(props: SearchInputProps) {
-  const { placeholder = "Search" } = props;
+  const { children, onChange = _.noop, placeholder = "Search", value } = props;
+
+  const hasSuggestions = useMemo(
+    () => children && !_.isEmpty(children),
+    [children]
+  );
+
+  const onSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    onChange(value);
+  };
 
   return (
     <div className="w-full">
@@ -17,8 +31,15 @@ export default function SearchInput(props: SearchInputProps) {
           type="search"
           className="block w-full h-[48px] py-1.5 pl-5 pr-10 bg-white rounded border-0 ring-1 ring-inset ring-black-200"
           placeholder={placeholder}
+          onChange={onSearchChange}
+          value={value}
         />
       </div>
+      {hasSuggestions && (
+        <div className="absolute mt-1.5 bg-white-100 shadow-[0px_0px_8px_rgba(26,26,30,0.2)] divide-y-2 divide-white-500">
+          {children}
+        </div>
+      )}
     </div>
   );
 }
