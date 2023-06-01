88e2ce00c8978460dcb23909f5d340a3cf52685c
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2023-05-31 18:16:01 -0700

    feat(ui): create search suggestion header component

---
+++ packages/ui/src/components/search-input/search-suggestion-header.tsx
@@ -0,0 +1,9 @@
+import {PropsWithChildren} from "react";
+
+export default function SearchSuggestionHeader({children}: PropsWithChildren) {
+  return (
+    <div className="flex justify-between px-5 py-2">
+        {children}
+    </div>
+  );
+}
