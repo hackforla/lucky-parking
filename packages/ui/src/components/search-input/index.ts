fd51af6f04091b89467a21ae67c2ef72283be3c1
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2023-05-31 18:16:56 -0700

    feat(ui): extend functionality of search input component

--- packages/ui/src/components/search-input/index.ts
+++ packages/ui/src/components/search-input/index.ts
@@ -1,1 +1,3 @@
 export { default } from "./search-input";
+export { default as SearchSuggestion } from "./search-suggestion";
+export { default as SearchSuggestionHeader } from "./search-suggestion-header";
