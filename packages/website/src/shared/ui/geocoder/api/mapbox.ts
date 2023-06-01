8a77ce50e99e3e3d7c81e4ac4e5f4b241888559b
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2023-05-31 18:17:44 -0700

    feat(website): create geocoder component

---
+++ packages/website/src/shared/ui/geocoder/api/mapbox.ts
@@ -0,0 +1,25 @@
+import _ from "lodash";
+import losAngelesFeature from "@/shared/data/geo/los-angeles.json";
+
+export const fetchGeocodingData = async (value: string) => {
+  const params = new URLSearchParams({
+    access_token: import.meta.env["VITE_MAPBOX_TOKEN"],
+    address_accuracy: "address",
+    // @ts-ignore
+    bbox: _.first(losAngelesFeature?.features).bbox.toString(),
+    country: "US",
+    fuzzyMatch: "true",
+    language: "en",
+    routing: "false",
+    types: "address,poi,postcode,neighborhood",
+    worldview: "us",
+  });
+  const res = await fetch(
+    `https://api.mapbox.com/geocoding/v5/mapbox.places/${value}.json?${params}`
+  );
+  return res.ok ? res.json() : undefined;
+};
+
+export default {
+  fetchGeocodingData,
+};
