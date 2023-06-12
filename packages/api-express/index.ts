31ce9852e6a81510a4be621d691829daa78bd887
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2023-06-12 15:42:23 -0700

    added connection to mongodb in express app

--- packages/api-express/index.ts
+++ packages/api-express/index.ts
@@ -1,10 +1,19 @@
 const express = require("express");
 const routes = require("./routes");
+import { connectToDatabase } from "./services/database.service"
+
 
 const PORT = 3000;
 
 const app = express();
 
-app.use("/", routes);
+connectToDatabase()
+    .then(() => {
+        app.use("/", routes);
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+        app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+    })
+    .catch((error: Error) => {
+        console.error("Database connection failed", error);
+        process.exit();
+    });
