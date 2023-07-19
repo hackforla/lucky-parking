0479a6b2ced38f7fdd54e86522a8fb9e9434342c
Gibson Han <gibsonthedev@gmail.com>
2023-07-18 20:03:52 -0700

    chore: setup jest and react testing library

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,27 @@
 import type { Config } from 'jest';
 
 const config: Config = {
+  roots: ["<rootDir>/packages/"],
   preset: 'ts-jest',
-  transform: {
-    '^.+\\.(ts|tsx)?$': ['ts-jest', { 
-      "tsconfig": "./packages/website/tsconfig.json"
-    }],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "!<rootDir>/node_modules",
+    "<rootDir>/packages/website/**/*.{ts,tsx}",
+  ],
+  modulePaths: ['<rootDir>/packages/website'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/packages/website/src/$1'
   },
-  'testEnvironment': 'jest-environment-jsdom',
-
+  transform: {
+    '^.+\\.(ts|tsx)?': ['ts-jest', {
+      tsconfig: "<rootDir>/packages/website/tsconfig.json"
+    }]
+  },
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/'],
+  setupFilesAfterEnv: [
+    "<rootDir>/jest.setup.ts"
+  ],
 };
 
 export default config
--- packages/website/src/widgets/header/header.test.tsx
+++ packages/website/src/widgets/header/header.test.tsx
@@ -3,18 +3,21 @@ import { render, screen } from '@testing-library/react'
 import Header from './header'
 
 describe('Header', () => {
-  test('renders the header with branding, description, and action', async () => {
+  beforeEach(() => {
     render(<Header />)
+  })
 
+  test('renders with branding', () => {
     expect(screen.getByRole('logo')).toBeInTheDocument()
-
+  })
+  test('renders with description', () => {
     //description 
     expect(screen.getByText('Los Angeles Parking Citation Data')).toBeInTheDocument();
     expect(screen.getByText('Helping L.A. make informed decisions about parking policies')).toBeInTheDocument();
+  })
 
-    //action button
+  test('renders with action button', () => {
     const button = screen.getByRole('button', { name: 'HOW IT WORKS' });
     expect(button).toBeInTheDocument();
-
   })
 })
