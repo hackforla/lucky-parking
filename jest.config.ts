19b76a55db20ba14509667a622278bd89c2145b2
Gibson Han <gibsonthedev@gmail.com>
2023-07-19 19:03:05 -0700

    chore: configure jest to load website tsconfig and load its alias

---
+++ jest.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  roots: ["<rootDir>/packages/"],
+  preset: 'ts-jest',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "!<rootDir>/node_modules",
+    "<rootDir>/packages/website/**/*.{ts,tsx}",
+  ],
+  modulePaths: ['<rootDir>/packages/website'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/packages/website/src/$1'
+  },
+  transform: {
+    '^.+\\.(ts|tsx)?': ['ts-jest', {
+      tsconfig: "<rootDir>/packages/website/tsconfig.json"
+    }]
+  },
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/'],
+  setupFilesAfterEnv: [
+    "<rootDir>/jest.setup.ts"
+  ],
+};
+
+export default config
