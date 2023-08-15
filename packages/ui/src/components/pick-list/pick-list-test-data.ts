56361cc385d7128971902388e4195bfe2de13846
Gibson Han <gibsonthedev@gmail.com>
2023-08-14 19:15:24 -0700

    feat(ui): Implement picklist

---
+++ packages/ui/src/components/pick-list/pick-list-test-data.ts
@@ -0,0 +1,8 @@
+export const LIST_OPTIONS = [
+  { value: "bmw", text: "BMW" },
+  { value: "chevrolet", text: "Chevrolet" },
+  { value: "ford", text: "Ford" },
+  { value: "honda", text: "Honda" },
+  { value: "toyota", text: "Toyota" },
+  { value: "none", text: "Dude, Where's my Car?" },
+];
