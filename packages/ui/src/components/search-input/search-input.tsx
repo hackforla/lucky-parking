64f5c5920690a01a754e1555b1d24644a08f4660
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2023-09-25 10:47:19 -0700

    feat(ui): update search input component

--- packages/ui/src/components/search-input/search-input.tsx
+++ packages/ui/src/components/search-input/search-input.tsx
@@ -3,18 +3,16 @@ import SearchIcon from "@mui/icons-material/Search";
 import { ChangeEvent, PropsWithChildren, useMemo, useState } from "react";
 
 interface SearchInputProps extends PropsWithChildren {
+  isDisabled?: boolean;
   onChange?: (arg0: any) => void;
   placeholder?: string;
   value: string;
 }
 
 export default function SearchInput(props: SearchInputProps) {
-  const { children, onChange = _.noop, placeholder = "Search", value } = props;
+  const { children, isDisabled = false, onChange = _.noop, placeholder = "Search", value } = props;
 
-  const hasSuggestions = useMemo(
-    () => children && !_.isEmpty(children),
-    [children],
-  );
+  const hasSuggestions = useMemo(() => children && !_.isEmpty(children), [children]);
 
   const onSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
@@ -24,21 +22,22 @@ export default function SearchInput(props: SearchInputProps) {
   return (
     <div className="w-full">
       <div className="relative">
-        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
-          <SearchIcon />
-        </div>
         <input
           type="search"
           className="ring-black-200 block h-[48px] w-full rounded border-0 bg-white py-1.5 pl-5 pr-10 ring-1 ring-inset"
           placeholder={placeholder}
           onChange={onSearchChange}
           value={value}
+          disabled={isDisabled}
         />
+        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+          <SearchIcon />
+        </div>
       </div>
       {hasSuggestions && (
-        <div className="bg-white-100 divide-white-500 absolute mt-1.5 divide-y-2 shadow-[0px_0px_8px_rgba(26,26,30,0.2)]">
+        <ul className="bg-white-100 divide-white-500 absolute z-[100] mt-1.5 divide-y-2 shadow-[0px_0px_8px_rgba(26,26,30,0.2)]">
           {children}
-        </div>
+        </ul>
       )}
     </div>
   );
