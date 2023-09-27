02941a6cceb56132133757124dfc3365a61f43f8
Gibson Han <gibsonthedev@gmail.com>
2023-09-26 21:33:45 -0700

    feat(website): implement radius-tool with picklist, input, and unit convertor

--- packages/website/src/pages/parking-insights-page.tsx
+++ packages/website/src/pages/parking-insights-page.tsx
@@ -2,6 +2,7 @@ import Map from "@/features/map";
 import Page from "@/pages/page";
 import Header from "@/widgets/header";
 import { CitationExplorer } from "@/widgets/citation";
+import { RadiusTool } from "@/widgets/radius-tool";
 
 const MAP_NAME = "parking-insights";
 
@@ -11,6 +12,9 @@ export default function ParkingInsightsPage() {
       <Header />
       <div className="bg-white-100 absolute left-4 top-20 z-40 flex w-[500px] flex-col">
         <CitationExplorer />
+      </div>
+      <div className="absolute left-1/2 top-20 z-40">
+        <RadiusTool isSubmitDisabled={false} onSubmit={(value) => console.log(value)} />
       </div>
       <Map name={MAP_NAME} />;
     </Page>
