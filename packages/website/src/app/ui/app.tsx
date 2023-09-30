e9db3c043f28b3f84c50cf83b54a7fb8e467d5bb
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2023-09-30 12:55:41 -0700

    feat(website): set up map provider

--- packages/website/src/app/ui/app.tsx
+++ packages/website/src/app/ui/app.tsx
@@ -1,12 +1,15 @@
 import { Provider as StoreProvider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
+import MapProvider from "@/entities/map/lib/provider/map-provider";
 import { store } from "@/shared/data";
 import router from "./routing";
 
 export function App() {
   return (
     <StoreProvider store={store}>
-      <RouterProvider router={router} />
+      <MapProvider>
+        <RouterProvider router={router} />
+      </MapProvider>
     </StoreProvider>
   );
 }
