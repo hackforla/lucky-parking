01b5f257d1393a9c0f895af73e46157023c7ac7a
Gibson Han <gibsonthedev@gmail.com>
2023-10-10 21:45:18 -0700

    feat(website): refactor stepper-container-component

--- packages/ui/src/components/stepper/stepper-container.tsx
+++ packages/ui/src/components/stepper/stepper-container.tsx
@@ -1,18 +1,28 @@
-import { ReactNode, createElement } from "react";
+import { Children, ReactElement, ReactNode, createElement } from "react";
 
 interface StepperContainerProps {
   currentStep?: number;
   children: ReactNode[];
 }
 
+const isReactElement = (child: ReactNode): child is ReactElement => {
+  return child !== null && typeof child === "object" && "type" in child;
+};
+
 export function StepperContainer({ currentStep = 0, children }: StepperContainerProps) {
-  const len = children?.length;
+  const items = Children.toArray(children) || [];
+  const len = items.length;
 
   return (
     <div className="ml-4 flex flex-col">
-      {children?.map((child: any, idx: number) => {
-        const isLast = idx === len - 1;
-        return createElement(child.type, { key: idx, ...child.props, isCurrent: currentStep === idx, isLast });
+      {items.map((child, idx) => {
+        if (!isReactElement(child)) return <></>;
+        return createElement(child.type, {
+          key: idx,
+          ...child.props,
+          isCurrent: currentStep === idx,
+          isLast: idx === len - 1,
+        });
       })}
     </div>
   );
