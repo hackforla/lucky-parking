e10d86188ad061607c9dc46b16e2b9e2f509ef4a
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2023-10-16 22:13:44 -0700

    test(website): fix broken tests

--- packages/website/src/widgets/header/header.test.tsx
+++ packages/website/src/widgets/header/header.test.tsx
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "@/shared/lib/utilities/testing";
 import Header from "./header";
 
 describe("Header", () => {
@@ -8,18 +8,12 @@ describe("Header", () => {
   });
 
   test("renders with branding", () => {
-    expect(screen.getByRole("logo")).toBeInTheDocument();
+    expect(screen.getByRole("img", { name: "Parking Insights brand mark" })).toBeInTheDocument();
   });
   test("renders with description", () => {
     //description
-    expect(
-      screen.getByText("Los Angeles Parking Citation Data"),
-    ).toBeInTheDocument();
-    expect(
-      screen.getByText(
-        "Helping L.A. make informed decisions about parking policies",
-      ),
-    ).toBeInTheDocument();
+    expect(screen.getByText("Los Angeles Parking Citation Data")).toBeInTheDocument();
+    expect(screen.getByText("Helping L.A. make informed decisions about parking policies")).toBeInTheDocument();
   });
 
   test("renders with action button", () => {
--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsConfigPaths from "vite-tsconfig-paths";
+import environment from "vite-plugin-environment";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsConfigPaths()],
+  plugins: [react(), tsConfigPaths(), environment("all")],
 });
