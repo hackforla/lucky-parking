aed4d52c9e4c0bddc825509c8ea83929bc4e4c43
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2023-10-16 22:09:47 -0700

    config: update testing configurations

--- jest.setup.js
+++ jest.setup.js
@@ -1,17 +1,13 @@
 import "@testing-library/jest-dom";
 
 /**
- * JSDOM doesn't implement PointerEvent so we need to mock our own implementation
+ * JSDOM doesn't implement PointerEvent.
  * Default to mouse left click interaction
  * https://github.com/radix-ui/primitives/issues/1822
  * https://github.com/jsdom/jsdom/pull/2666
  */
 class MockPointerEvent extends Event {
-  button: number;
-  ctrlKey: boolean;
-  pointerType: string;
-
-  constructor(type: string, props: PointerEventInit) {
+  constructor(type, props) {
     super(type, props);
     this.button = props.button || 0;
     this.ctrlKey = props.ctrlKey || false;
@@ -19,7 +15,7 @@ class MockPointerEvent extends Event {
   }
 }
 
-window.PointerEvent = MockPointerEvent as any;
+window.PointerEvent = MockPointerEvent;
 window.HTMLElement.prototype.scrollIntoView = jest.fn();
 window.HTMLElement.prototype.releasePointerCapture = jest.fn();
 window.HTMLElement.prototype.hasPointerCapture = jest.fn();
