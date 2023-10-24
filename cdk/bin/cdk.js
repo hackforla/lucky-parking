1b04266a22b927fc019b8b435b8d4ca69b8cf20b
Andrewgl22 <ledermanmusic@gmail.com>
2023-10-24 12:20:43 -0400

    fix

--- cdk/bin/cdk.js
+++ cdk/bin/cdk.js
@@ -1,17 +1,16 @@
 #!/usr/bin/env node
 
-const cdk = require('aws-cdk-lib');
-const { EcrDockerStack } = require('../lib/docker-stack');
-const { MyElasticBeanstalkStack } = require('../lib/eb-stack');
+const cdk = require("aws-cdk-lib");
+const { EcrDockerStack } = require("../lib/docker-stack");
+const { MyElasticBeanstalkStack } = require("../lib/eb-stack");
 
 const app = new cdk.App();
 
 // Initialize your Docker stack
-const dockerStack = new EcrDockerStack(app, 'MyEcrDockerStack');
+const dockerStack = new EcrDockerStack(app, "MyEcrDockerStack");
 
 // Initialize your Elastic Beanstalk stack
 // You can also pass in properties from the Docker stack if needed
-new MyElasticBeanstalkStack(app, 'MyElasticBeanstalkStack', {
-  uiRepo: dockerStack.uiEcrRepo,
-  websiteRepo: dockerStack.websiteEcrRepo
+new MyElasticBeanstalkStack(app, "MyElasticBeanstalkStack", {
+  websiteRepo: dockerStack.websiteEcrRepo,
 });
