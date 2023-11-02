425f7c067d309d2defd78e92596d6375682a4131
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2023-11-02 16:54:34 -0700

    feat(api-express): implement api versioning

--- packages/api-express/index.ts
+++ packages/api-express/index.ts
@@ -5,9 +5,10 @@ import routes from "./routes";
 import "./database/db";
 
 const { PORT = 3000 } = process.env;
+const VERSION = "v1";
 
 const app = express();
 
-app.use("/", routes);
+app.use(`/${VERSION}`, routes);
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
