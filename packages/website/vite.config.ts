49a154a916639c2478c6bbc78dfe70324b83d98f
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2023-11-06 19:15:20 -0800

    fix(website): load environment variables properly for production

--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tsConfigPaths from "vite-tsconfig-paths";
 import environment from "vite-plugin-environment";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsConfigPaths(), environment("all")],
+export default defineConfig((config) => {
+  const { mode } = config;
+
+  return {
+    define: { "process.env": loadEnv(mode, process.cwd(), "") },
+    plugins: [react(), tsConfigPaths(), environment("all")],
+  };
 });
