960f3e92aa69b6c69215de3583a300ee460669b7
bennyv8 <bvanhr@gmail.com>
2023-11-17 17:43:05 -0800

    re-factor citation-data-category-selection test
    Seperated tests into two describe blocks for rendering and interactions
    Added test for selecting option

--- packages/website/src/features/citation/ui/citation-data-category-selection.test.tsx
+++ packages/website/src/features/citation/ui/citation-data-category-selection.test.tsx
@@ -3,24 +3,47 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import CitationDataCategorySelection from "./citation-data-category-selection";
-import { CitationDataCategories } from "../lib";
 
-describe("Citation Data Category Selection", () => {
-  const options = Object.values(CitationDataCategories);
+describe("Citation-data-category-selection rendering", () => {
   beforeEach(() => {
     render(<CitationDataCategorySelection onSelect={() => {}} />);
   });
 
-  test("renders citation data categories with initial placeholder text", async () => {
-    expect(screen.getByText(options[0]));
+  test("should render citation data categories with initial placeholder", async () => {
+    expect(screen.getByText("Total # Citations"));
   });
 
-  test("should allow user to change the current citation data category", async () => {
+  test("should render all citation data categories options", async () => {
     const user = userEvent.setup();
 
-    expect(screen.queryByText("Total $ Fine Amount")).toBe(null);
     await user.click(screen.getByRole("combobox"));
-    await user.click(screen.getByText(options[1]));
-    expect(screen.getByText(options[1])).toBeInTheDocument();
+    expect(screen.getByRole("option", { name: "Total # Citations" }));
+    expect(screen.getByRole("option", { name: "Citations per Sq. Mile (Density)" }));
+    expect(screen.getByRole("option", { name: "Violation Type" }));
+    expect(screen.getByRole("option", { name: "Total $ Fine Amount" }));
+    expect(screen.getByRole("option", { name: "Fine $ Amount per Sq. Mile (Density)" }));
+    expect(screen.getByRole("option", { name: "Day of the Week" }));
+  });
+});
+
+describe("Citation-data-category-selection interactions", () => {
+  const onSelectMock = jest.fn();
+  beforeEach(async () => {
+    const user = userEvent.setup();
+    render(<CitationDataCategorySelection onSelect={onSelectMock} />);
+    await user.click(screen.getByRole("combobox"));
+    await user.click(screen.getByRole("option", { name: "Day of the Week" }));
+  });
+
+  test("should not render with default option", () => {
+    expect(screen.queryByText("Total # Citations")).toBe(null);
+  });
+
+  test("should render with selected option", async () => {
+    expect(screen.getByText("Day of the Week"));
+  });
+
+  test("should return selected option", async () => {
+    expect(onSelectMock).toHaveBeenCalledWith("Day of the Week");
   });
 });
