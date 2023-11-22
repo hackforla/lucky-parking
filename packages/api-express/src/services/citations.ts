37ce3671d798258aea3fd18df52604ce2f209a7c
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2023-11-21 17:43:28 -0800

    fix(api-express): update query to find citations

--- packages/api-express/src/services/citations.ts
+++ packages/api-express/src/services/citations.ts
@@ -1,23 +1,17 @@
 import { db } from "../database";
 import { CitationFilters } from "../utilities/types";
 
-// STUB
+const { COL_NAME_CITATIONS } = process.env;
+
 const findCitations = async (filters: CitationFilters) => {
   const { dates, geometry } = filters;
 
   const query = {
-    $and: [
-      {
-        dates: {
-          $gte: dates[0],
-          $lte: dates[1],
-        },
-      },
-      { geometry },
-    ],
+    $and: [{ issue_date: { $gte: dates[0], $lte: dates[1] } }, { geometry: { $geoWithin: { $geometry: geometry } } }],
   };
+
   try {
-    const collection = db.collection("citations");
+    const collection = db.collection(COL_NAME_CITATIONS as string);
     return await collection.find(query).toArray();
   } catch (error: any) {
     throw new Error(error.message);
