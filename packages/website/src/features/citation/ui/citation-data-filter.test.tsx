9159abda158f633b36b316547fd33186675adfbb
Christian Rodriguez <christian.jro25@gmail.com>
2023-11-22 16:25:48 -0800

    Add test coverage for citation-data-filter interactions with the citation-data-categories and citation-date-presets fields

--- packages/website/src/features/citation/ui/citation-data-filter.test.tsx
+++ packages/website/src/features/citation/ui/citation-data-filter.test.tsx
@@ -16,6 +16,7 @@ describe("Citation-data-filter rendering", () => {
   test("should render citation date presets with initial placeholder", async () => {
     expect(screen.getByText("Past 1 Year"));
   });
+
   test("should render date inputs with initial placeholders", async () => {
     const fromInput = screen.getByText("From");
     const toInput = screen.getByText("To");
@@ -28,5 +29,35 @@ describe("Citation-data-filter rendering", () => {
 
     expect(fromValue).toHaveTextContent("mm/dd/yy");
     expect(toValue).toHaveTextContent("mm/dd/yy");
+  });
+});
+
+describe("Citation-data-filter interactions", () => {
+  const onCategorySelectMock = jest.fn();
+  const onDatePresetSelectMock = jest.fn();
+  beforeEach(async () => {
+    const user = userEvent.setup();
+    render(<CitationDataFilter onCategorySelect={onCategorySelectMock} onDatePresetSelect={onDatePresetSelectMock} />);
+
+    await user.click(screen.getByRole("combobox", { name: "citation-data-categories" }));
+    await user.click(screen.getByRole("option", { name: "Violation Type" }));
+
+    await user.click(screen.getByRole("combobox", { name: "citation-date-presets" }));
+    await user.click(screen.getByRole("option", { name: "Year to Date" }));
+  });
+
+  test("should not render with default options", () => {
+    expect(screen.queryByText("Total # Citations")).toBe(null);
+    expect(screen.queryByText("Past 1 Year")).toBe(null);
+  });
+
+  test("should render with selected options", async () => {
+    expect(screen.getByText("Violation Type"));
+    expect(screen.getByText("Year to Date"));
+  });
+
+  test("should return selected options", async () => {
+    expect(onCategorySelectMock).toHaveBeenCalledWith("Violation Type");
+    expect(onDatePresetSelectMock).toHaveBeenCalledWith("Year to Date");
   });
 });
