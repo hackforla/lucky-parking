411a26c8da3670c980234934a8e9c4fa6baf2196
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2023-11-25 16:16:58 -0800

    feat(website): invoke user event setup with custom render for testing

--- packages/website/src/shared/lib/utilities/testing/testing.tsx
+++ packages/website/src/shared/lib/utilities/testing/testing.tsx
@@ -1,12 +1,15 @@
-import { render as renderRTL } from "@testing-library/react";
 import { PropsWithChildren, ReactElement } from "react";
+import { render as renderRTL } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import TestWrapper from "./test-wrapper";
 
 const createWrapper =
   (props = {}) =>
   ({ children }: PropsWithChildren) => <TestWrapper {...props}>{children}</TestWrapper>;
 
-export const render = (ui: ReactElement, { routes = ["/"], ...options } = {}) =>
-  renderRTL(ui, { wrapper: createWrapper({ routes }), ...options });
+export const render = (ui: ReactElement, { routes = ["/"], ...options } = {}) => ({
+  user: userEvent.setup(),
+  ...renderRTL(ui, { wrapper: createWrapper({ routes }), ...options }),
+});
 
 export * from "@testing-library/react";
