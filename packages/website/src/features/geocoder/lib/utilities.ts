98c730677fed84908433d777764dfbcd0779120f
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2023-12-09 15:28:28 -0800

    refactor(website)!: refactor website to consume utilities package
    BREAKING-CHANGE: Many of the utility functions in the `website` package have been deprecated and removed in favor of those in the `utilities` package.

--- packages/website/src/features/geocoder/lib/utilities.ts
+++ packages/website/src/features/geocoder/lib/utilities.ts
@@ -1,9 +1,9 @@
 import _ from "lodash";
+import { isSubstring } from "@lucky-parking/utilities/dist/string";
 import type { Bbox } from "@mapbox/mapbox-gl-geocoder";
 import { bbox, center } from "@turf/turf";
 import neighborhoodCouncils from "@/shared/data/feature-collections/los-angeles-neighborhood-councils.json";
 import { GeocodeResult } from "@/shared/lib/types";
-import { isSubstring } from "@/shared/lib/utilities/string";
 import { PlaceType } from "../lib/constants";
 
 export const forwardGeocodeNeighborhoodCouncil = (query: string) => {
