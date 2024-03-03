27d48f4303e8a0f31d4fe971047a902ab7e3abae
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2024-03-02 18:41:03 -0800

    fix: resolve formatting problems

--- packages/ui/src/components/calendar/utils/is-equal.ts
+++ packages/ui/src/components/calendar/utils/is-equal.ts
@@ -1,13 +1,6 @@
 import { CalendarDate } from "./create-calendar";
 
-export function isEqual(
-  { day, month, year }: CalendarDate,
-  targetDate: Date | null,
-): boolean {
+export function isEqual({ day, month, year }: CalendarDate, targetDate: Date | null): boolean {
   if (!targetDate) return false;
-  return (
-    targetDate.getDate() === day &&
-    targetDate.getMonth() === month &&
-    targetDate.getFullYear() === year
-  );
+  return targetDate.getDate() === day && targetDate.getMonth() === month && targetDate.getFullYear() === year;
 }
