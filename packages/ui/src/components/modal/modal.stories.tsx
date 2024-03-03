5077c8113f78fa53954c1f2188f2579470da0631
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2024-03-02 18:41:03 -0800

    fix: resolve formatting problems

--- packages/ui/src/components/modal/modal.stories.tsx
+++ packages/ui/src/components/modal/modal.stories.tsx
@@ -42,9 +42,11 @@ export const CustomTitleAndDescription: Content = {
           <p>Custom</p>
         </Button>
       </ModalTrigger>
-      <ModalContent className='w-[200px]'>
-        <ModalTitle className='text-3xl mb-4'>Kamina</ModalTitle>
-        <ModalDescription className='mb-3'>Believe In The <span className='font-bold'>Me</span> That Believe In <span className="font-bold">You</span></ModalDescription>
+      <ModalContent className="w-[200px]">
+        <ModalTitle className="mb-4 text-3xl">Kamina</ModalTitle>
+        <ModalDescription className="mb-3">
+          Believe In The <span className="font-bold">Me</span> That Believe In <span className="font-bold">You</span>
+        </ModalDescription>
         <ModalClose asChild>
           <Button variant={ButtonVariant.primary}>
             <p>Ok!</p>
