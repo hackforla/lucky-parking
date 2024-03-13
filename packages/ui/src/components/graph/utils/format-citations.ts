a0bf0ecd3e5a4ff25c71053256a55030b92481dd
bennyv8 <bvanhr@gmail.com>
2024-03-13 16:57:34 -0700

    Updated formatCitations to be re-usable
    - will format citations according to category argument.
    - utilizes constants to keep uniformity

--- packages/ui/src/components/graph/utils/format-citations.ts
+++ packages/ui/src/components/graph/utils/format-citations.ts
@@ -1,3 +1,6 @@
+import {ViolationTypeCategories, violationTypeBarData, dayOfWeekBarData} from "../lib"
+
+
 interface Citation {
   _id: string;
   agency: string;
@@ -28,24 +31,38 @@ interface Citation {
 
 interface CitationBarDatum {
   [key: string]: string | number;
-  violation_description: string;
+  index_key: string;
   count: number;
 }
 
-export function formatCitations(payload: Citation[]): CitationBarDatum[] {
+export function formatCitations(category: string, citations: Citation[]): CitationBarDatum[] {
   interface Recorder {
     [key: string]: CitationBarDatum;
   }
 
   const recorder: Recorder = {};
-  return payload.reduce((acc: CitationBarDatum[], payload) => {
-    if (recorder[payload.violation_description]) {
-      recorder[payload.violation_description]["count"]++;
-    } else {
-      const newRecord = { violation_description: payload.violation_description, count: 1 };
-      recorder[payload.violation_description] = newRecord;
-      acc.push(recorder[payload.violation_description]);
+
+  if (category === "Violation Type") {
+    for (let i = 0; i < violationTypeBarData.length; i++ ) {
+      const violation = violationTypeBarData[i].index_key
+      recorder[violation] = violationTypeBarData[i];
     }
-    return acc;
-  }, []);
+    for (let i = 0; i < citations.length; i++) {
+      const violation_description = citations[i].violation_description as keyof typeof ViolationTypeCategories;
+      recorder[ViolationTypeCategories[violation_description]].count++;
+    }
+    return violationTypeBarData;
+  } else if (category === "Day of the Week") {
+    for (let i = 0; i < dayOfWeekBarData.length; i++) {
+      const dayOfWeek = dayOfWeekBarData[i].index_key
+      recorder[dayOfWeek] = dayOfWeekBarData[i]
+    }
+    for (let i = 0; i < citations.length; i++) {
+      const day = new Date(citations[i].issue_date).toLocaleDateString('en-US', { weekday: 'long' });
+      recorder[day].count++
+    }
+    return dayOfWeekBarData;
+  }
+
+  return [];
 }
