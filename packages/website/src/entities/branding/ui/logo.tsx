0123eaffd5098b6f4d4a52fb8bf4b05f8cd9ba11
Christian Rodriguez Ochoa <christian.jro25@gmail.com>
2024-03-15 15:42:01 -0700

    Update header icon to clear search query params and reload page.

--- packages/website/src/entities/branding/ui/logo.tsx
+++ packages/website/src/entities/branding/ui/logo.tsx
@@ -1,8 +1,15 @@
 import BrandMark from "./brandmark.svg";
+import type { onEvent } from "@lucky-parking/typings";
 
-export default function Logo() {
+type LogoProps = {
+  onClick?: onEvent;
+};
+
+export default function Logo(props: LogoProps) {
+  const { onClick } = props;
+
   return (
-    <div className="flex items-center space-x-2">
+    <div className="flex items-center space-x-2" onClick={onClick}>
       <img src={BrandMark} alt="Parking Insights brand mark" />
 
       <div className="select-none -space-y-1 font-semibold uppercase text-blue-600">
--- packages/website/src/widgets/header/header.tsx
+++ packages/website/src/widgets/header/header.tsx
@@ -2,14 +2,22 @@ import { Link } from "react-router-dom";
 import { Logo } from "@/entities/branding";
 import { PATHS } from "@/entities/navigation";
 import HowItWorksButton from "@/features/instructions/how-it-works-button";
+import useCitationSearchParams from "@/features/citation/ui/use-citation-search-params";
 
 export default function Header() {
+  const citationSearchParams = useCitationSearchParams();
+
+  const handleLogoClick = () => {
+    citationSearchParams.clearSearchParams();
+    window.location.reload();
+  };
+
   return (
     <div className="flex h-[60px] flex-1 items-center justify-between px-5 py-2">
       {/* Branding */}
       <div className="flex flex-1 items-center space-x-6">
         <Link to={PATHS.HOME}>
-          <Logo />
+          <Logo onClick={handleLogoClick} />
         </Link>
 
         {/* Description */}
