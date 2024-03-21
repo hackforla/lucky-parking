e9e262e17df994414af360074a1032f4cffa3ff1
bzzz-coding <86077274+bzzz-coding@users.noreply.github.com>
2024-03-20 20:23:16 -0700

    Changed reloadDocument to true

--- packages/website/src/entities/branding/ui/logo.tsx
+++ packages/website/src/entities/branding/ui/logo.tsx
@@ -7,7 +7,7 @@ export default function Logo() {
      * reloadDocument - A property used in the Link component from React Router to skip client side routing and let the browser handle the transition normally (as if it were an <a href>).
      * @typedef {boolean} reloadDocument
      */
-    <Link to="/" reloadDocument={false} data-testid="logo" className="flex items-center space-x-2">
+    <Link to="/" reloadDocument={true} data-testid="logo" className="flex items-center space-x-2">
       <img src={BrandMark} alt="Parking Insights brand mark" />
 
       <div className="select-none -space-y-1 font-semibold uppercase text-blue-600">
