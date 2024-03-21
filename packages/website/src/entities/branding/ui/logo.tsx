77dc53c200986f33e8f7fa245599162ddd2a9df4
bzzz-coding <86077274+bzzz-coding@users.noreply.github.com>
2024-03-20 17:55:15 -0700

    Added link to home page to Logo

--- packages/website/src/entities/branding/ui/logo.tsx
+++ packages/website/src/entities/branding/ui/logo.tsx
@@ -1,14 +1,19 @@
 import BrandMark from "./brandmark.svg";
+import { Link } from "react-router-dom";
 
 export default function Logo() {
   return (
-    <div className="flex items-center space-x-2">
+    /**
+     * reloadDocument - A property used in the Link component from React Router to skip client side routing and let the browser handle the transition normally (as if it were an <a href>).
+     * @typedef {boolean} reloadDocument
+     */
+    <Link to="/" reloadDocument={false} data-testid="logo" className="flex items-center space-x-2">
       <img src={BrandMark} alt="Parking Insights brand mark" />
 
       <div className="select-none -space-y-1 font-semibold uppercase text-blue-600">
         <p>Parking</p>
         <p>Insights</p>
       </div>
-    </div>
+    </Link>
   );
 }
