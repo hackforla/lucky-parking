2ac60156a5dcccead18a18ba9011cadc7cdccebf
Benny <62785232+bennyv8@users.noreply.github.com>
2024-03-29 21:27:01 -0700

    Build Bar Graph Component (#632)
    * Added Bar Grpah for Citations by Violation Code
    
    - Implemented a utility function to abstract payload data
    - Created a sample bar graph component to best closely resemble figma
    designs
    	a. Note, unable to create bolded number next to "total #
    citations" on x axis
    - Add component to storybook
    
    * Install depend @nivo/core @nivo/bar - FORCED
    
    - I think eslint is linting the pnpm file so I had to force the commit
    
    * Bar Data Structure
    
    - To keep x-axis uniformity when loading data since the order of the
    x-axis is loaded sequentially
    
    * Add re-usability for horizontal bar graph component
    
    - Component takes in category and payload to organize graph citations
    - Added types
    - Fixed spelling error
    
    * Updated sample payload for rendering
    
    - fixed naming convention from citations -> payload
    - added additional citations and changed dates to render all 7 days
    
    * Updated formatCitations to be re-usable
    
    - will format citations according to category argument.
    - utilizes constants to keep uniformity
    
    * Added option to change categories in storybook
    
    * Rename variables and improve accessibility
    
    - Change names to better reflect purpose
    - Improve aria descriptions for accessibility
    - left comment for future dev

---
+++ packages/ui/src/components/graph/utils/format-citations.ts
@@ -0,0 +1,67 @@
+import { ViolationTypeCategories, violationTypeBarData, dayOfWeekBarData } from "../lib";
+
+interface Citation {
+  _id: string;
+  agency: string;
+  agency_desc: string;
+  body_style: string;
+  body_style_desc: string;
+  color: string;
+  color_desc: string;
+  fine_amount: string;
+  issue_date: string;
+  issue_time: string;
+  latitude: string;
+  location: string;
+  longitude: string;
+  make: string;
+  marked_time: string;
+  plate_expiry_date: string;
+  rp_state_plate: string;
+  ticket_number: string;
+  violation_code: string;
+  violation_description: string;
+  route: string;
+  geometry: {
+    type: string;
+    coordinates: number[];
+  };
+}
+
+interface CitationBarDatum {
+  [key: string]: string | number;
+  index_key: string;
+  count: number;
+}
+
+export function formatBarGraphCitations(category: string, citations: Citation[]): CitationBarDatum[] {
+  interface CategoryMap {
+    [key: string]: CitationBarDatum;
+  }
+
+  const categoryMap: CategoryMap = {};
+
+  if (category === "Violation Type") {
+    for (let i = 0; i < violationTypeBarData.length; i++) {
+      const violation = violationTypeBarData[i].index_key;
+      categoryMap[violation] = violationTypeBarData[i];
+    }
+    for (let i = 0; i < citations.length; i++) {
+      const violation_description = citations[i].violation_description as keyof typeof ViolationTypeCategories;
+      categoryMap[ViolationTypeCategories[violation_description]].count++;
+    }
+    return violationTypeBarData;
+  } else if (category === "Day of the Week") {
+    for (let i = 0; i < dayOfWeekBarData.length; i++) {
+      const dayOfWeek = dayOfWeekBarData[i].index_key;
+      categoryMap[dayOfWeek] = dayOfWeekBarData[i];
+    }
+    for (let i = 0; i < citations.length; i++) {
+      const day = new Date(citations[i].issue_date).toLocaleDateString("en-US", { weekday: "long" });
+      categoryMap[day].count++;
+    }
+    return dayOfWeekBarData;
+  }
+
+  return [];
+}
