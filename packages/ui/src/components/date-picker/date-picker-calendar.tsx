4a8dcb569c199af702d45a4ac8c1628e9fdf1a65
Gibson Han <gibsonthedev@gmail.com>
2024-04-08 15:44:08 -0700

    fix ui: center calendar titles

--- packages/ui/src/components/date-picker/date-picker-calendar.tsx
+++ packages/ui/src/components/date-picker/date-picker-calendar.tsx
@@ -72,118 +72,127 @@ export default function DatePickerCalendar(props: DatePickerCalendarProps) {
 
   return (
     <div data-testid="date-range-calendar" className="bg-white-100 w-[512px]">
-      {/**Calendar Month Nav */}
-      <div className="flex h-[52px] flex-1 justify-between px-4">
-        <div className="flex flex-1 items-center justify-start space-x-4">
-          <button aria-label="prev-months" className="px-2" onClick={() => handleUpdateMonth("prev")}>
-            <ArrowBackIosNewIcon sx={{ fontSize: 8, color: "#7A7A7B" }} />
-          </button>
-          <div className="space-x-1">
-            <span aria-label="first-date-month"> {MONTH_NAMES[month]} </span>
-            <span aria-label="first-date-year">{year}</span>
+      <div className="flex w-full items-center justify-center">
+        <div className="flex-col items-center justify-center">
+          {/*Left Calendar Nav*/}
+          <div className="flex h-[52px] flex-1 items-center justify-between space-x-4 px-4">
+            <button aria-label="prev-months" className="px-2" onClick={() => handleUpdateMonth("prev")}>
+              <ArrowBackIosNewIcon sx={{ fontSize: 8, color: "#7A7A7B" }} />
+            </button>
+            <div className="space-x-1">
+              <span aria-label="first-date-month"> {MONTH_NAMES[month]} </span>
+              <span aria-label="first-date-year">{year}</span>
+            </div>
+            <span className="invisible h-6 w-6" />
+          </div>
+
+          {/*Left Calendar*/}
+          <div className="flex justify-center px-4 pb-2">
+            <table className="border-collaspse">
+              <thead>
+                <tr>
+                  {["S", "M", "T", "W", "T", "F", "S"].map((ele: string, index: number) => (
+                    <td
+                      key={ele + index}
+                      className="text-black-400 h-8 w-8 p-px text-center text-xs font-normal leading-[16.8px]">
+                      {ele}
+                    </td>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {firstMonth.map((week: CALENDAR_MONTH[], weekIdx: number) => (
+                  <tr key={"first-month" + weekIdx}>
+                    {week.map((ele: CALENDAR_MONTH, colIdx: number) => {
+                      if (isEmpty(ele)) {
+                        return <td key={"empty" + colIdx} className="h-8 w-8" />;
+                      }
+                      const { day, month, year } = ele as CalendarDate;
+                      const key = `first-month-${month}/${day}/${year}`;
+                      const isSelected =
+                        isEqual(ele as CalendarDate, startDate) || isEqual(ele as CalendarDate, endDate);
+                      const isCurrDate = isEqual(ele as CalendarDate, initDate);
+
+                      return (
+                        <td
+                          key={key}
+                          onClick={() => handleSelected(ele as CalendarDate)}
+                          className={twMerge(
+                            "h-8 w-8 text-center text-xs font-normal leading-[18.8px]",
+                            isCurrDate && "inline-flex items-center justify-center rounded-full border-[1px]",
+                            !isSelected && "rounded-full hover:bg-blue-200",
+                            isSelected && "text-white-100 rounded-full bg-blue-500",
+                          )}>
+                          {day}
+                        </td>
+                      );
+                    })}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
         </div>
 
-        <div className="flex flex-1 items-center justify-end space-x-4">
-          <div className="space-x-1">
-            <span aria-label="second-date-month"> {MONTH_NAMES[(month + 1) % 12]} </span>
-            <span aria-label="second-date-year"> {(month + 1) % 12 === 0 ? year + 1 : year} </span>
+        <div className="flex-col items-center justify-center">
+          {/**Right Calendar Nav */}
+          <div className="flex h-[52px] flex-1 items-center justify-between space-x-4 px-4">
+            <span className="invisible h-6 w-6" />
+            <div className="space-x-1">
+              <span aria-label="second-date-month"> {MONTH_NAMES[(month + 1) % 12]} </span>
+              <span aria-label="second-date-year"> {(month + 1) % 12 === 0 ? year + 1 : year} </span>
+            </div>
+            <button aria-label="next-months" className="px-2" onClick={() => handleUpdateMonth("next")}>
+              <ArrowForwardIosIcon sx={{ fontSize: 8, color: "#7A7A7B" }} />
+            </button>
           </div>
-          <button aria-label="next-months" className="px-2" onClick={() => handleUpdateMonth("next")}>
-            <ArrowForwardIosIcon sx={{ fontSize: 8, color: "#7A7A7B" }} />
-          </button>
-        </div>
-      </div>
 
-      {/** Actual Calendars */}
-      <div className="flex w-full">
-        <div className="flex justify-center px-4 pb-2">
-          <table className="border-collaspse">
-            <thead>
-              <tr>
-                {["S", "M", "T", "W", "T", "F", "S"].map((ele: string, index: number) => (
-                  <td
-                    key={ele + index}
-                    className="text-black-400 h-8 w-8 p-px text-center text-xs font-normal leading-[16.8px]">
-                    {ele}
-                  </td>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              {firstMonth.map((week: CALENDAR_MONTH[], weekIdx: number) => (
-                <tr key={"first-month" + weekIdx}>
-                  {week.map((ele: CALENDAR_MONTH, colIdx: number) => {
-                    if (isEmpty(ele)) {
-                      return <td key={"empty" + colIdx} className="h-8 w-8" />;
-                    }
-                    const { day, month, year } = ele as CalendarDate;
-                    const key = `first-month-${month}/${day}/${year}`;
-                    const isSelected = isEqual(ele as CalendarDate, startDate) || isEqual(ele as CalendarDate, endDate);
-                    const isCurrDate = isEqual(ele as CalendarDate, initDate);
-
-                    return (
-                      <td
-                        key={key}
-                        onClick={() => handleSelected(ele as CalendarDate)}
-                        className={twMerge(
-                          "h-8 w-8 text-center text-xs font-normal leading-[18.8px]",
-                          isCurrDate && "inline-flex items-center justify-center rounded-full border-[1px]",
-                          !isSelected && "rounded-full hover:bg-blue-200",
-                          isSelected && "text-white-100 rounded-full bg-blue-500",
-                        )}>
-                        {day}
-                      </td>
-                    );
-                  })}
+          {/**Right Calendar*/}
+          <div className="flex justify-center px-4 pb-2">
+            <table className="border-collaspse">
+              <thead>
+                <tr>
+                  {["S", "M", "T", "W", "T", "F", "S"].map((ele: string, index: number) => (
+                    <td
+                      key={ele + index}
+                      className="text-black-400 h-8 w-8 p-px text-center text-xs font-normal leading-[16.8px]">
+                      {ele}
+                    </td>
+                  ))}
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-        <div className="flex justify-center px-4 pb-2">
-          <table className="border-collaspse">
-            <thead>
-              <tr>
-                {["S", "M", "T", "W", "T", "F", "S"].map((ele: string, index: number) => (
-                  <td
-                    key={ele + index}
-                    className="text-black-400 h-8 w-8 p-px text-center text-xs font-normal leading-[16.8px]">
-                    {ele}
-                  </td>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              {secondMonth.map((week: CALENDAR_MONTH[], weekIdx: number) => (
-                <tr key={"second-month" + weekIdx}>
-                  {week.map((ele: CALENDAR_MONTH, colIdx: number) => {
-                    if (isEmpty(ele)) {
-                      return <td key={"empty" + colIdx} className="h-8 w-8" />;
-                    }
-                    const { day, month, year } = ele as CalendarDate;
-                    const key = `second-month-${month}/${day}/${year}`;
-                    const isSelected = isEqual(ele as CalendarDate, startDate) || isEqual(ele as CalendarDate, endDate);
-                    const isCurrDate = isEqual(ele as CalendarDate, initDate);
+              </thead>
+              <tbody>
+                {secondMonth.map((week: CALENDAR_MONTH[], weekIdx: number) => (
+                  <tr key={"second-month" + weekIdx}>
+                    {week.map((ele: CALENDAR_MONTH, colIdx: number) => {
+                      if (isEmpty(ele)) {
+                        return <td key={"empty" + colIdx} className="h-8 w-8" />;
+                      }
+                      const { day, month, year } = ele as CalendarDate;
+                      const key = `second-month-${month}/${day}/${year}`;
+                      const isSelected =
+                        isEqual(ele as CalendarDate, startDate) || isEqual(ele as CalendarDate, endDate);
+                      const isCurrDate = isEqual(ele as CalendarDate, initDate);
 
-                    return (
-                      <td
-                        key={key}
-                        onClick={() => handleSelected(ele as CalendarDate)}
-                        className={twMerge(
-                          "h-8 w-8 text-center text-xs font-normal leading-[18.8px]",
-                          isCurrDate && "inline-flex items-center justify-center rounded-full border-[1px]",
-                          !isSelected && "rounded-full hover:bg-blue-200",
-                          isSelected && "text-white-100 rounded-full bg-blue-500",
-                        )}>
-                        {day}
-                      </td>
-                    );
-                  })}
-                </tr>
-              ))}
-            </tbody>
-          </table>
+                      return (
+                        <td
+                          key={key}
+                          onClick={() => handleSelected(ele as CalendarDate)}
+                          className={twMerge(
+                            "h-8 w-8 text-center text-xs font-normal leading-[18.8px]",
+                            isCurrDate && "inline-flex items-center justify-center rounded-full border-[1px]",
+                            !isSelected && "rounded-full hover:bg-blue-200",
+                            isSelected && "text-white-100 rounded-full bg-blue-500",
+                          )}>
+                          {day}
+                        </td>
+                      );
+                    })}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
