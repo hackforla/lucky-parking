02a0231f52f3d2562b4db8f2f4057bba3cc32680
Gibson Han <gibsonthedev@gmail.com>
2024-04-08 16:00:01 -0700

    fix ui: rename suggestion radio test

--- packages/ui/src/components/date-picker/date-picker-suggestions.test.tsx
+++ packages/ui/src/components/date-picker/date-picker-suggestions.test.tsx
@@ -2,16 +2,16 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import DatePickerSuggestions from "./date-picker-suggestions";
 
-describe("Date Input", () => {
+describe("Date Range Suggestions Radio Group", () => {
   beforeEach(() => {
     render(<DatePickerSuggestions onSuggestionChange={(value) => console.log(value)} />);
   });
 
   test("renders radio group and all it's options", () => {
     expect(screen.getByLabelText("Past 1 Year"));
+    expect(screen.getByLabelText("Past 3 Months"));
+    expect(screen.getByLabelText("Past 1 Month"));
     expect(screen.getByLabelText("Year to Date"));
-    expect(screen.getByLabelText("3 Months"));
-    expect(screen.getByLabelText("1 Month"));
     expect(screen.queryByLabelText("It's over 9000")).not.toBeInTheDocument();
   });
 });
