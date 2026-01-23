eb236c31ed316bc28787c49569edcc81a27d8988
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2026-01-22 17:52:08 -0800

    build(web): containerize app

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+	output: "standalone",
 	redirects: () => [{ source: "/", destination: "/parking-insights", permanent: false }],
 };
 
