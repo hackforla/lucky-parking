b37b0cb5a0c082ac2d16250021a8cde9f7ef4066
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2026-03-02 15:28:53 -0800

    Fix integration workflow (#714)
    * chore: sort tasks alphabetically
    
    * fix: restore format command to workspace
    
    * fix: remove format commands from api app
    
    * fix: remove config flag from projects to allow prettier config discovery
    
    * fix: use mjs for prettier config files
    
    * chore: format
    
    * fix: fix typo
    
    * chore: update prettier dependencies
    
    * chore: upgrade to node@24

--- packages/configs/prettier.config.ts
+++
@@ -1,1 +0,0 @@
-export { config as default } from "./src/prettier/prettier.config.ts";
--- packages/design/prettier.config.d.ts
+++
@@ -1,2 +0,0 @@
-export { config as default } from "@lucky-parking/configs/prettier";
-//# sourceMappingURL=prettier.config.d.ts.map
