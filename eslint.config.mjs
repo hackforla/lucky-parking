cc8ebf77a9d220a1ceb572f435fe737c1f3ed356
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2026-09-06 00:17:43 -0700

    chore: use turbo to manage tasks and execution

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+	...nextVitals,
+	...nextTs,
+	{
+		rules: {
+			"@typescript-eslint/ban-ts-comment": [
+				"error",
+				{
+					"ts-check": false,
+					"ts-expect-error": "allow-with-description",
+					"ts-ignore": "allow-with-description",
+					"ts-nocheck": false,
+				},
+			],
+			"import/no-anonymous-default-export": "warn",
+			"react-hooks/purity": "warn",
+			"react-hooks/set-state-in-effect": "warn",
+		},
+	},
+	globalIgnores([
+		"**/node_modules/**",
+		"**/.turbo/**",
+		"**/.next/**",
+		"**/out/**",
+		"**/build/**",
+		"**/dist/**",
+		"**/coverage/**",
+		"**/next-env.d.ts",
+	]),
+]);
---
+++ prettier.config.mjs
@@ -0,0 +1,57 @@
+/**
+ * @type {import('prettier').Config}
+ * @see https://prettier.io/docs/configuration
+ */
+export default {
+	/**
+	 * Prettier options
+	 * @see https://prettier.io/docs/options
+	 */
+	arrowParens: "always",
+	bracketSameLine: true,
+	bracketSpacing: true,
+	checkIgnorePragma: true,
+	embeddedLanguageFormatting: "auto",
+	endOfLine: "lf",
+	experimentalOperatorPosition: "end",
+	experimentalTernaries: false,
+	htmlWhitespaceSensitivity: "css",
+	insertPragma: false,
+	jsxSingleQuote: false,
+	objectWrap: "preserve",
+	printWidth: 120,
+	proseWrap: "always",
+	quoteProps: "as-needed",
+	requirePragma: false,
+	semi: true,
+	singleAttributePerLine: true,
+	singleQuote: false,
+	tabWidth: 2,
+	trailingComma: "es5",
+	useTabs: true,
+
+	/**
+	 * Plugins
+	 */
+	plugins: [
+		"@homer0/prettier-plugin-jsdoc",
+		"@trivago/prettier-plugin-sort-imports",
+		"@xeonlink/prettier-plugin-organize-attributes",
+		"prettier-plugin-css-order",
+		"prettier-plugin-tailwindcss", // Must load last
+	],
+
+	/**
+	 * @trivago/prettier-plugin-sort-imports options
+	 * @see https://github.com/trivago/prettier-plugin-sort-imports?tab=readme-ov-file#usage
+	 */
+	importOrder: [
+		"^^\\\u0000", // Side-effect imports
+		"^(fs|path|crypto|os|http|https|child_process|events|util|url)$", // Node.js modules
+		"<THIRD_PARTY_MODULES>", // Third-party modules
+		"^@/.+$", // Internal imports
+		"^\.\./.+$", // Parent imports
+		"^\\\\./.+$", // Sibling imports
+		"\\.css$", // CSS imports
+	],
+};
